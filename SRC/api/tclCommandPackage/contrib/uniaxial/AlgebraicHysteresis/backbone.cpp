3bb19207b5a3320232dcfcebc5a7dff341757425
claudio perez <50180406+claudioperez@users.noreply.github.com>
2022-11-29 20:29:54 -0800

    cmp

--- SRC/api/tclCommandPackage/contrib/uniaxial/AlgebraicHysteresis/backbone.cpp
+++ SRC/api/tclCommandPackage/contrib/uniaxial/AlgebraicHysteresis/backbone.cpp
@@ -11,7 +11,7 @@
 // TCL hystereticBackbone command.
 
 #include <OPS_Globals.h>
-#include <InputAPI.h>
+#include <api/InputAPI.h>
 #include <UniaxialMaterial.h>
 #include <runtime/BasicModelBuilder.h>
 
