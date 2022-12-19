083e6d67b004b9a1b9bd1fa27411abc9987a61eb
claudio perez <50180406+claudioperez@users.noreply.github.com>
2022-12-18 23:01:12 -0800

    cmp

--- BuildTools/cmake/OpenSeesDependenciesWin.cmake
+++ BuildTools/cmake/OpenSeesDependenciesWin.cmake
@@ -23,12 +23,17 @@ set(CONDA_DIR "C:/Users/claud/miniconda3")
 set(CONDA_ENV "C:/Users/claud/miniconda3/envs/sim")
 set(BUNDLE_LIBS "${PROJECT_SOURCE_DIR}/Win64/lib/debug/")
 
-opensees_load(TCL CONAN tcl/8.6.10)
+opensees_load(TCL CONAN tcl/8.6.11)
 
 set(TCL_INCLUDE_PATH ${TCL_INCLUDE_DIRS})
 set(TCL_LIBRARIES ${TCL_LIBRARY})
 
 message("TCL: ${TCL_INCLUDE_PATH}")
+
+# opensees_load(MySQL CONAN mysql-connector-c/6.1.11
+#     #LIBRARY ${CONDA_ENV}/Library/lib/libmysql.lib
+#     #INCLUDE ${CONDA_ENV}/Library/include/mysql
+# )
 
 opensees_load(BLAS
   LIBRARY "${BUNDLE_LIBS}/blas.lib"
@@ -38,44 +43,41 @@ opensees_load(CBLAS
   LIBRARY "${BUNDLE_LIBS}/cblas.lib"
 )
 
-opensees_load(UMFPACK
-  #LIBRARY "${BUNDLE_LIBS}/umfpackC.lib"
-  LIBRARY ${OPS_BUNDLED_DIR}/bin/UMFPACK/Debug/UMFPACK.lib
-)
-
-opensees_load(CSPARSE
-  #LIBRARY "${BUNDLE_LIBS}/csparse.lib"
-  LIBRARY ${OPS_BUNDLED_DIR}/bin/CSPARSE/Debug/CSPARSE.lib
-)
-
 opensees_load(LAPACK
   LIBRARY "${BUNDLE_LIBS}/lapack.lib"
 )
 
-#set(ENV{SUPERLU_DIR})
-opensees_load(SUPERLU
-  #BUNDLED ${OPS_BUNDLED_DIR}/SuperLU_5.1.1/
-  LIBRARY ${OPS_BUNDLED_DIR}/bin/SuperLU_5.1.1/Debug/SUPERLU.lib
-)
+# find_package(BLAS)
 
-opensees_load(ARPACK                                       SEARCH
-  #BUNDLED ${OPS_BUNDLED_DIR}/ARPACK/
-  LIBRARY ${OPS_BUNDLED_DIR}/bin/ARPACK/Debug/ARPACK.lib
-)
-
-opensees_load(AMD
-  #BUNDLED ${OPS_BUNDLED_DIR}/AMD/
-  LIBRARY ${OPS_BUNDLED_DIR}/bin/AMD/Debug/AMD.lib
-)
+# opensees_load(UMFPACK
+#   #LIBRARY "${BUNDLE_LIBS}/umfpackC.lib"
+#   LIBRARY ${OPS_BUNDLED_DIR}/bin/UMFPACK/Debug/UMFPACK.lib
+# )
+# 
+# opensees_load(CSPARSE
+#   #LIBRARY "${BUNDLE_LIBS}/csparse.lib"
+#   LIBRARY ${OPS_BUNDLED_DIR}/bin/CSPARSE/Debug/CSPARSE.lib
+# )
+# 
+# #set(ENV{SUPERLU_DIR})
+# opensees_load(SUPERLU
+#   #BUNDLED ${OPS_BUNDLED_DIR}/SuperLU_5.1.1/
+#   LIBRARY ${OPS_BUNDLED_DIR}/bin/SuperLU_5.1.1/Debug/SUPERLU.lib
+# )
+# 
+# opensees_load(ARPACK                                       SEARCH
+#   #BUNDLED ${OPS_BUNDLED_DIR}/ARPACK/
+#   LIBRARY ${OPS_BUNDLED_DIR}/bin/ARPACK/Debug/ARPACK.lib
+# )
+# 
+# opensees_load(AMD
+#   #BUNDLED ${OPS_BUNDLED_DIR}/AMD/
+#   LIBRARY ${OPS_BUNDLED_DIR}/bin/AMD/Debug/AMD.lib
+# )
 
 opensees_load(METIS                                        SEARCH)
 
 opensees_load(HDF5                                           FIND)
-
-opensees_load(MySQL CONAN mysql-connector-c/6.1.11
-    #LIBRARY ${CONDA_ENV}/Library/lib/libmysql.lib
-    #INCLUDE ${CONDA_ENV}/Library/include/mysql
-)
 
 # set(MYSQL_INCLUDE_DIR "${CONDA_ENV}/Library/include/mysql/")
 
