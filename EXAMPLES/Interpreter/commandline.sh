72f855f9e0281ea7188b5648d529470ba0c70664
claudio perez <50180406+claudioperez@users.noreply.github.com>
2023-06-20 17:20:41 -0700

    cmp - work on commandline test

--- EXAMPLES/Interpreter/commandline.sh
+++ EXAMPLES/Interpreter/commandline.sh
@@ -1,18 +1,21 @@
 
 # run file, then drop into a REPL
-python -m opensees -i truss.tcl
+opensees -i truss.tcl
 
 # run file 'args.tcl'. Output should be 'a b c'
-python -m opensees args.tcl a b c
+opensees args.tcl a b c
 
 # This should be a file not found error
-cat args.tcl | python -m opensees a b c
+cat args.tcl | opensees a b c
+
+#
+cat args.tcl | opensees -c 'set argv newargs'
 
 # run command 'set argv 3'. Output should be '3' on stdout
-python -m opensees -c 'puts "[set argv 3]"'
+opensees -c 'puts "[set argv 3]"'
 
 # run command 'set argv 3', then read file 'args.tcl', then drop into a REPL.
 # The command should override argv and the output should be '3' on stdout
-python -m opensees -c 'set argv 3' args.tcl a b c
+opensees -c 'set argv 3' args.tcl a b c
 
 
