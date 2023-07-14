92b10c656f856c38721ebc1d81787bc4664c8b62
claudio perez <50180406+claudioperez@users.noreply.github.com>
2023-07-14 09:52:37 -0700

    cmp

---
+++ EXAMPLES/WikiScripts/RUNITALL.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+Results="README.md"
+
+cat - > $Results <<EOF
+# Examples
+
+| Status  |     File     |  Message              |
+|---------|--------------|-----------------------|
+EOF
+
+ls $@ | grep -v -e "Proc.*" -e Dispwall1-cg.tcl -e Ex6.genericFrame[23]D.build.InelasticFiber -e "Lib.*" -e TestSlider -e "Read.*" -e "Ex.*\.analyze\..*" -e "^PUL.*" -e "CenterCol[A-Z]" -e '.*_input_.*' | while read i; do
+  echo "FILE $i"; 
+  msg="$(python -m opensees $i 2>&1 >/dev/null)";
+  code="$?"
+  echo "$msg"
+  msg=$(tail -1 <<< "$msg")
+  printf "|  $code  |"' `'"$i"'`'" | $msg |\n"  >> $Results; #| tee -a $Results;
+done
+
