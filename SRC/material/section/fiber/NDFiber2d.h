942fa8ed2f1ec32dbec6784e77fc03d846817a1b
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-01-03 22:10:30 -0800

    cmp - banner style

--- SRC/material/section/fiber/NDFiber2d.h
+++ SRC/material/section/fiber/NDFiber2d.h
@@ -17,12 +17,7 @@
 **   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
 **                                                                    **
 ** ****************************************************************** */
-                                                                        
-// $Revision$
-// $Date$
-// $Source$
-
-
+//
 // File: ~/fiber/NDFiber2d.h
 //
 // Written: MHS
