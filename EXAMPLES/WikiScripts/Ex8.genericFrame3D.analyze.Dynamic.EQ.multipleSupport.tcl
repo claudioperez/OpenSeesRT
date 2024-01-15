0a80d3b4fa09b886580a26701b710c8cfaee2f11
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-01-14 20:35:07 -0800

    cmp

--- EXAMPLES/WikiScripts/Ex8.genericFrame3D.analyze.Dynamic.EQ.multipleSupport.tcl
+++ EXAMPLES/WikiScripts/Ex8.genericFrame3D.analyze.Dynamic.EQ.multipleSupport.tcl
@@ -11,7 +11,7 @@ source ReadSMDFile.tcl;		# procedure for reading GM file and converting it to pr
 set iSupportNode [lrange $iSupportNode 0 3];	# choose only the first 4 support nodes, defined with model
 set iGMfact "1.0 1.1 1.2 1.3"
 set iGMdirection "1 1 1 1"
-set iGMfile "H-e12140 H-e12140 H-e12140 H-e12140"
+set iGMfile "H-E12140 H-E12140 H-E12140 H-E12140"
 
 # Define DISPLAY -------------------------------------------------------------
 # the deformed shape is defined in the build file
