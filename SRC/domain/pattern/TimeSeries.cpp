4c0011a585d5316124bbdea47ebfaac74940ec7b
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-01-19 14:43:30 -0800

    cmp - remove some global `OPS_*` functions

--- SRC/domain/pattern/TimeSeries.cpp
+++ SRC/domain/pattern/TimeSeries.cpp
@@ -17,56 +17,21 @@
 **   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
 **                                                                    **
 ** ****************************************************************** */
-                                                                        
-// $Revision: 1.5 $
-// $Date: 2010-02-16 18:57:05 $
-// $Source: /usr/local/cvs/OpenSees/SRC/domain/pattern/TimeSeries.cpp,v $
-                                                                        
-                                                                        
-// Written: fmk 
-// Created: 07/99
-// Revision: A
 //
 // Purpose: This file contains the class definition for TimeSeries.
 // TimeSeries is an abstract class. A TimeSeries object is used to
 // determine the load factor to be applied to the loads in a pettern.
-// to the model. 
+// to the model.
 //
-// What: "@(#) TimeSeries.C, revA"
-
+// Written: fmk 
+// Created: 07/99
+// Revision: A
+//
 #include <TimeSeries.h>
 #include <TaggedObject.h>
 #include <MapOfTaggedObjects.h>
 
 static MapOfTaggedObjects theTimeSeriesObjects;
-
-#if !defined(OPS_USE_RUNTIME)
-bool OPS_addTimeSeries(TimeSeries *newComponent) {
-  return theTimeSeriesObjects.addComponent(newComponent);
-}
-
-bool OPS_removeTimeSeries(int tag) {
-    TaggedObject* obj = theTimeSeriesObjects.removeComponent(tag);
-    if (obj != 0) {
-	delete obj;
-	return true;
-    }
-    return false;
-}
-
-TimeSeries *OPS_getTimeSeries(int tag)
-{
-
-  TaggedObject *theResult = theTimeSeriesObjects.getComponentPtr(tag);
-  if (theResult == 0) {
-    opserr << "TimeSeries *getTimeSeries(int tag) - none found with tag: " << tag << endln;
-    return 0;
-  }
-  TimeSeries *theSeries = (TimeSeries *)theResult;
-
-  return theSeries->getCopy();
-}
-#endif // OPS_USE_RUNTIME
 
 void
 OPS_ADD_RUNTIME_VXV(OPS_clearAllTimeSeries)
