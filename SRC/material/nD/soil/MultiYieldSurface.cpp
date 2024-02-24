830c543a58d7986f2fa88902bf846f26fb06494e
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-02-24 15:26:42 -0800

    cleaning

--- SRC/material/nD/soil/MultiYieldSurface.cpp
+++ SRC/material/nD/soil/MultiYieldSurface.cpp
@@ -1,17 +1,14 @@
-// $Revision: 1.8 $
-// $Date: 2003-02-14 23:01:30 $
-// $Source: /usr/local/cvs/OpenSees/SRC/material/nD/soil/MultiYieldSurface.cpp,v $
-                                                                        
+//
 // Written: ZHY
 // Created: August 2000
 //
 // MultiYieldSurface.cpp
 // ---------------------
 //
-
+#include <assert.h>
 #include <math.h>
 #include <stdlib.h>
-
+#include <OPS_ErrorStream.h>
 #include <MultiYieldSurface.h>
 
 
@@ -44,11 +41,7 @@ void MultiYieldSurface::setData(const Vector & theCenter_init,
 
 void MultiYieldSurface::setCenter(const Vector & newCenter)
 {
-  if (newCenter.Size() != 6) {
-    opserr << "FATAL:MultiYieldSurface::setCenter(Vector &): vector size not equal 6" << endln;
-    exit(-1);
-  }
-
+  assert(newCenter.Size() == 6);
   theCenter = newCenter;
 }
 
@@ -74,13 +67,14 @@ istream & operator>> (istream & is, MultiYieldSurface & a)
 
 double secondOrderEqn(double A, double B, double C, int i)
 {
-  if(A == 0){
+  if (A == 0) {
     opserr << "FATAL:second_order_eqn: A=0." << endln;
     if(i==0) opserr << " when finding reference point on outer surface." <<endln;
     else opserr << " when moving active surface." <<endln;
     exit(-1);   
   }
-  if(C == 0) return 0;
+  if(C == 0) 
+    return 0;
   if(B == 0){
     if(C/A > 0){
       opserr << "FATAL:second_order_eqn: Complex roots.\n";
@@ -89,39 +83,50 @@ double secondOrderEqn(double A, double B, double C, int i)
     return sqrt(-C/A);
   }
 
-	double determ, val1, val2, val;
+  double determ, val1, val2, val;
   determ = B*B - 4.*A*C; 
   if(determ < 0){
     opserr << "FATAL:second_order_eqn: Complex roots.\n";
-    if(i==0) opserr << " when finding reference point on outer surface." <<endln;
-    else opserr << " when moving active surface." <<endln;
+    if (i==0) 
+      opserr << " when finding reference point on outer surface." <<endln;
+    else 
+      opserr << " when moving active surface." <<endln;
+
     opserr << "B2=" << B*B << " 4AC=" << 4.*A*C <<endln; 
     exit(-1);
   }
   
-  if (B > 0) val1 = (-B - sqrt(determ)) / (2.*A);
-  else val1 = (-B + sqrt(determ)) / (2.*A);
+  if (B > 0) 
+    val1 = (-B - sqrt(determ)) / (2.*A);
+  else
+    val1 = (-B + sqrt(determ)) / (2.*A);
+
   val2 = C / (A * val1);
 
-  if (val1 < 0 && val2 < 0){
+  if (val1 < 0 && val2 < 0) {
 		if (fabs(val1) < LOW_LIMIT) val1 = 0.;
 		else if (fabs(val2) < LOW_LIMIT) val2 = 0.;
-	}
+  }
 
   if (val1 < 0 && val2 < 0){
     opserr << "FATAL:second_order_eqn: Negative roots.\n";
-    if(i==0) opserr << " when finding reference point on outer surface." <<endln;
-    else opserr << " when moving active surface." <<endln;
-		opserr << "A=" << A << " B=" << B << " C=" << C << " det=" << determ << 
-			" x1=" << val1 << " x2=" << val2 << endln;  
+    if (i==0) 
+      opserr << " when finding reference point on outer surface." <<endln;
+    else 
+      opserr << " when moving active surface." <<endln;
+    opserr << "A=" << A << " B=" << B << " C=" << C << " det=" << determ << 
+            " x1=" << val1 << " x2=" << val2 << endln;  
     exit(-1);   
   }
   
-  if (val1 < 0) return  val2;
-  else if (val2 < 0) return  val1;
+  if (val1 < 0) 
+    return  val2;
+  else if (val2 < 0) 
+    return  val1;
   else{
     val = val1;
-    if(val > val2)  val = val2;
-		return val;
+    if(val > val2)
+      val = val2;
+    return val;
   }
 }
