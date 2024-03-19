855cbe7bcaae74115bf92ab637a83927581b1d23
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-03-18 20:41:45 -0700

    cmp - clean, use `getTypedObject<>` API

--- SRC/element/Frame/ForceInterp/ForceBeamColumnWarping2d.cpp
+++ SRC/element/Frame/ForceInterp/ForceBeamColumnWarping2d.cpp
@@ -143,14 +143,14 @@ void * OPS_ADD_RUNTIME_VPV(OPS_ForceBeamColumnWarping2d)
     }
 
     // check transf
-    CrdTransf* theTransf = OPS_getCrdTransf(iData[3]);
+    CrdTransf* theTransf = G3_getSafeBuilder(rt)->getTypedObject<CrdTransf>(iData[3]);
     if(theTransf == 0) {
 	opserr<<"coord transfomration not found\n";
 	return 0;
     }
 
     // check beam integrataion
-    BeamIntegrationRule* theRule = (BeamIntegrationRule*)(G3_getSafeBuilder(rt)->getRegistryObject("BeamIntegrationRule", iData[4]));
+    BeamIntegrationRule* theRule = G3_getSafeBuilder(rt)->getTypedObject<BeamIntegrationRule>(iData[4]);
     if(theRule == 0) {
 	opserr<<"beam integration not found\n";
 	return 0;
