aff620b5a0352b898e3eb5b959c37a7c4d366bb0
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-03-30 18:56:43 -0700

    cmp - add `getScheme()`

--- SRC/material/section/SectionForceDeformation.h
+++ SRC/material/section/SectionForceDeformation.h
@@ -55,6 +55,20 @@ enum {
         SECTION_RESPONSE_W  = 10, // (FiberSectionWarping3d)
 };
 
+enum FrameKeys : unsigned long {
+  N     = 1<< 0, // 0b00000001
+  Vy    = 1<< 1, // 0b00000010
+  Vz    = 1<< 2, // 0b00000100
+  T     = 1<< 3, // 0b00000000
+  My    = 1<< 4, // 0b00000000
+  Mz    = 1<< 5, // 0b00000000
+  R     = 1<< 6, // 0b00000000
+  Q     = 1<< 7, // 0b00000000
+  B     = 1<< 8, // 0b00000000
+  W     = 1<< 9, // 0b00000000
+  End   = 1<<10
+};
+
 // section responses for shells
 enum {
         SECTION_RESPONSE_FXX = 11, // membrane xx
@@ -93,6 +107,7 @@ class SectionForceDeformation : public TaggedObject, public MovableObject
   virtual SectionForceDeformation *getCopy (void) = 0;
   virtual const ID &getType(void) = 0;
   virtual int getOrder (void) const = 0;
+  virtual unsigned long getScheme(void) const {return FrameKeys::End;}; // = 0;
   
   virtual Response *setResponse(const char **argv, int argc, OPS_Stream &s);
   virtual int getResponse(int responseID, Information &info);
