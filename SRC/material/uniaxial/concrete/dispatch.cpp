6b34082a62bd78b5065579b0ce0bda3eb3610e2e
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-04-17 15:17:52 -0700

    cmp - change to use `tcl.h`

--- SRC/material/uniaxial/concrete/dispatch.cpp
+++ SRC/material/uniaxial/concrete/dispatch.cpp
@@ -1,9 +1,12 @@
-#include <InputAPI.h>
+#include <tcl.h>
+#include <OPS_Globals.h>
+#include <BasicModelBuilder.h>
 #include <Concrete04.h>
 #include <Concrete06.h>
 #include <Concrete07.h>
 
-UniaxialMaterial* G3Parse_newUniaxialConcrete04(G3_Runtime* rt, int argc, G3_Char ** const argv)
+int 
+TclCommand_newUniaxialConcrete04(ClientData cd, Tcl_Interp* interp, int argc, TCL_Char ** const argv)
 {
     UniaxialMaterial *theMaterial = nullptr;
     int tag;
@@ -14,57 +17,57 @@ UniaxialMaterial* G3Parse_newUniaxialConcrete04(G3_Runtime* rt, int argc, G3_Cha
       opserr << "Want: uniaxialMaterial Concrete04 tag? fpc? epsc0? epscu? "
                 "Ec0? <ft? etu? <beta?> >"
              << endln;
-      return nullptr;
+      return TCL_ERROR;
     }
 
 
-    if (G3Parse_getInt(rt, argv[2], &tag) != TCL_OK) {
+    if (Tcl_GetInt(interp, argv[2], &tag) != TCL_OK) {
       opserr << "WARNING invalid uniaxialMaterial Concrete04 tag" << endln;
-      return nullptr;
+      return TCL_ERROR;
     }
 
     // Read required Concrete04 material parameters
 
-    if (G3Parse_getDouble(rt, argv[3], &fpc) != TCL_OK) {
+    if (Tcl_GetDouble(interp, argv[3], &fpc) != TCL_OK) {
       opserr << "WARNING invalid fpc\n";
       opserr << "Concrete04 material: " << tag << endln;
-      return nullptr;
+      return TCL_ERROR;
     }
 
-    if (G3Parse_getDouble(rt, argv[4], &epsc0) != TCL_OK) {
+    if (Tcl_GetDouble(interp, argv[4], &epsc0) != TCL_OK) {
       opserr << "WARNING invalid epsc0\n";
       opserr << "Concrete04 material: " << tag << endln;
-      return nullptr;
+      return TCL_ERROR;
     }
 
-    if (G3Parse_getDouble(rt, argv[5], &epscu) != TCL_OK) {
+    if (Tcl_GetDouble(interp, argv[5], &epscu) != TCL_OK) {
       opserr << "WARNING invalid epscu\n";
       opserr << "Concrete04 material: " << tag << endln;
-      return nullptr;
+      return TCL_ERROR;
     }
 
-    if (G3Parse_getDouble(rt, argv[6], &Ec0) != TCL_OK) {
+    if (Tcl_GetDouble(interp, argv[6], &Ec0) != TCL_OK) {
       opserr << "WARNING invalid Ec0\n";
       opserr << "Concrete04 material: " << tag << endln;
-      return nullptr;
+      return TCL_ERROR;
     }
     if (argc == 9 || argc == 10) {
-      if (G3Parse_getDouble(rt, argv[7], &ft) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[7], &ft) != TCL_OK) {
         opserr << "WARNING invalid ft\n";
         opserr << "Concrete04 material: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
-      if (G3Parse_getDouble(rt, argv[8], &etu) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[8], &etu) != TCL_OK) {
         opserr << "WARNING invalid etu\n";
         opserr << "Concrete04 material: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
     }
     if (argc == 10) {
-      if (G3Parse_getDouble(rt, argv[9], &beta) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[9], &beta) != TCL_OK) {
         opserr << "WARNING invalid beta\n";
         opserr << "Concrete04 material: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
     }
 
@@ -79,12 +82,14 @@ UniaxialMaterial* G3Parse_newUniaxialConcrete04(G3_Runtime* rt, int argc, G3_Cha
     } else if (argc == 7) {
       theMaterial = new Concrete04(tag, fpc, epsc0, epscu, Ec0);
     }
-    //return G3_addUniaxialMaterial(G3_getRuntime(interp), theMaterial);
-    return theMaterial;
+
+    return ((BasicModelBuilder*)cd)->addTaggedObject<UniaxialMaterial>(*theMaterial);
+//  return theMaterial;
 }
 
 
-UniaxialMaterial* G3Parse_newUniaxialConcrete06(G3_Runtime* rt, int argc, G3_Char ** const argv)
+int
+TclCommand_newUniaxialConcrete06(ClientData cd, Tcl_Interp* interp, int argc, TCL_Char ** const argv)
 {
   UniaxialMaterial *theMaterial = nullptr;
       if (argc < 12) {
@@ -93,84 +98,83 @@ UniaxialMaterial* G3Parse_newUniaxialConcrete06(G3_Runtime* rt, int argc, G3_Cha
             << "Want: uniaxialMaterial Concrete06 tag? fc? eo? r? k? alphaC? "
                "fcr? ecr? b? alphaT?"
             << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
       int tag;
 
-      if (G3Parse_getInt(rt, argv[2], &tag) != TCL_OK) {
+      if (Tcl_GetInt(interp, argv[2], &tag) != TCL_OK) {
         opserr << "WARNING invalid uniaxialMaterial Concrete06 tag" << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
       // Read required Concrete01 material parameters
       double fc, eo, r, k, fcr, ecr, b, alphaC, alphaT;
 
-      if (G3Parse_getDouble(rt, argv[3], &fc) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[3], &fc) != TCL_OK) {
         opserr << "WARNING invalid fc\n";
         opserr << "Concrete06 material: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
-      if (G3Parse_getDouble(rt, argv[4], &eo) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[4], &eo) != TCL_OK) {
         opserr << "WARNING invalid eo\n";
         opserr << "Concrete06 material: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
-      if (G3Parse_getDouble(rt, argv[5], &r) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[5], &r) != TCL_OK) {
         opserr << "WARNING invalid r\n";
         opserr << "Concrete06 material: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
-      if (G3Parse_getDouble(rt, argv[6], &k) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[6], &k) != TCL_OK) {
         opserr << "WARNING invalid k\n";
         opserr << "Concrete06 material: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
-      if (G3Parse_getDouble(rt, argv[7], &alphaC) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[7], &alphaC) != TCL_OK) {
         opserr << "WARNING invalid alphaC\n";
         opserr << "Concrete06 material: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
-      if (G3Parse_getDouble(rt, argv[8], &fcr) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[8], &fcr) != TCL_OK) {
         opserr << "WARNING invalid fcr\n";
         opserr << "Concrete06 material: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
-      if (G3Parse_getDouble(rt, argv[9], &ecr) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[9], &ecr) != TCL_OK) {
         opserr << "WARNING invalid ecr\n";
         opserr << "Concrete06 material: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
-      if (G3Parse_getDouble(rt, argv[10], &b) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[10], &b) != TCL_OK) {
         opserr << "WARNING invalid b\n";
         opserr << "Concrete06 material: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
-      if (G3Parse_getDouble(rt, argv[11], &alphaT) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[11], &alphaT) != TCL_OK) {
         opserr << "WARNING invalid alphaT\n";
         opserr << "Concrete06 material: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
       // Parsing was successful, allocate the material
       theMaterial =
           new Concrete06(tag, fc, eo, r, k, alphaC, fcr, ecr, b, alphaT);
 
-    //return G3_addUniaxialMaterial(G3_getRuntime(interp), theMaterial);
-    return theMaterial;
+    return ((BasicModelBuilder*)cd)->addTaggedObject<UniaxialMaterial>(*theMaterial);
+//  return theMaterial;
 }
 
-UniaxialMaterial*
-G3Parse_newUniaxialConcrete07(G3_Runtime* rt, int argc, G3_Char ** const argv)
-/* else if (strcmp(argv[1], "Concrete07") == 0) */ 
+int
+TclCommand_newUniaxialConcrete07(ClientData cd, Tcl_Interp* interp, int argc, TCL_Char ** const argv)
 {
     UniaxialMaterial *theMaterial = 0;
       // Check to see if there are enough arquements
@@ -178,64 +182,64 @@ G3Parse_newUniaxialConcrete07(G3_Runtime* rt, int argc, G3_Char ** const argv)
         opserr << "WARNING: Insufficient arguments\n";
         opserr << "Want: uniaxialMaterial Concrete07 tag? fpc? epsc0? Ec? fpt? "
                   "epst0? xcrp? xcrn? r?\n";
-        return nullptr;
+        return TCL_ERROR;
       }
 
       int tag;
 
-      if (G3Parse_getInt(rt, argv[2], &tag) != TCL_OK) {
+      if (Tcl_GetInt(interp, argv[2], &tag) != TCL_OK) {
         opserr << "WARNING: Invalid uniaxial Concrete07 tag\n";
-        return nullptr;
+        return TCL_ERROR;
       }
 
       // Read in the faluves required for the model
       double fpc, epsc0, Ec, fpt, epst0, xcrp, xcrn, r;
 
-      if (G3Parse_getDouble(rt, argv[3], &fpc) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[3], &fpc) != TCL_OK) {
         opserr << "WARNING: Invalid peak compression stress\n";
         opserr << "uniaxialMaterial Concrete07: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
-      if (G3Parse_getDouble(rt, argv[4], &epsc0) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[4], &epsc0) != TCL_OK) {
         opserr << "WARNING: Invalid peak compression strain\n";
         opserr << "uniaxialMaterial Concrete07: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
-      if (G3Parse_getDouble(rt, argv[5], &Ec) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[5], &Ec) != TCL_OK) {
         opserr << "WARNING: Invalid Young's Modulus\n";
         opserr << "uniaxialMaterial Concrete07: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
-      if (G3Parse_getDouble(rt, argv[6], &fpt) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[6], &fpt) != TCL_OK) {
         opserr << "WARNING: Invalid peak tension stress\n";
         opserr << "uniaxialMaterial Concrete07: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
-      if (G3Parse_getDouble(rt, argv[7], &epst0) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[7], &epst0) != TCL_OK) {
         opserr << "WARNING: Invalid peak tension strain\n";
         opserr << "uniaxialMaterial Concrete07: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
-      if (G3Parse_getDouble(rt, argv[8], &xcrp) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[8], &xcrp) != TCL_OK) {
         opserr
             << "WARNING: Invalid critical nondimensional strain in tension\n";
         opserr << "uniaxialMaterial Concrete07: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
-      if (G3Parse_getDouble(rt, argv[9], &xcrn) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[9], &xcrn) != TCL_OK) {
         opserr << "WARNING: Invalid critical nondimensional strain in "
                   "compression\n";
         opserr << "uniaxialMaterial Concrete07: " << tag << endln;
-        return nullptr;
+        return TCL_ERROR;
       }
 
-      if (G3Parse_getDouble(rt, argv[10], &r) != TCL_OK) {
+      if (Tcl_GetDouble(interp, argv[10], &r) != TCL_OK) {
         opserr << "WARNING: Invalid value for r\n";
         opserr << "uniaxialMaterial Concrete07: " << tag << endln;
       }
@@ -244,6 +248,6 @@ G3Parse_newUniaxialConcrete07(G3_Runtime* rt, int argc, G3_Char ** const argv)
       theMaterial =
           new Concrete07(tag, fpc, epsc0, Ec, fpt, epst0, xcrp, xcrn, r);
 
-    //return G3_addUniaxialMaterial(G3_getRuntime(interp), theMaterial);
-    return theMaterial;
+    return ((BasicModelBuilder*)cd)->addTaggedObject<UniaxialMaterial>(*theMaterial);
+//  return theMaterial;
 }   
