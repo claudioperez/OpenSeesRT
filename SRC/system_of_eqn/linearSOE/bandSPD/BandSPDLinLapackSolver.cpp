da17274cf1c5919beb2d3c08e148edce221e65ec
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-04-23 15:17:49 -0700

    cmp - cleaning

--- SRC/system_of_eqn/linearSOE/bandSPD/BandSPDLinLapackSolver.cpp
+++ SRC/system_of_eqn/linearSOE/bandSPD/BandSPDLinLapackSolver.cpp
@@ -17,76 +17,35 @@
 **   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
 **                                                                    **
 ** ****************************************************************** */
-                                                                        
-// $Revision: 1.5 $
-// $Date: 2009-05-20 17:30:26 $
-// $Source: /usr/local/cvs/OpenSees/SRC/system_of_eqn/linearSOE/bandSPD/BandSPDLinLapackSolver.cpp,v $
-                                                                        
-                                                                        
-// Written: fmk 
-// Created: 11/96
 //
 // Description: This file contains the implementation of BandSPDLinLapackSolver.
 //
-// What: "@(#) BandSPDLinLapackSolver.h, revA"
-
-#include <BandSPDLinLapackSolver.h>
-#include <BandSPDLinSOE.h>
-//#include <f2c.h>
+// Written: fmk
+// Created: 11/96
+//
 #include <assert.h>
 #include <math.h>
+#include <BandSPDLinLapackSolver.h>
+#include <BandSPDLinSOE.h>
+#include <blasdecl.h>
 
-void* OPS_BandSPDLinLapack()
-{
-    BandSPDLinSolver *theSolver = new BandSPDLinLapackSolver();
-    BandSPDLinSOE *theSOE = new BandSPDLinSOE(*theSolver);
-    return theSOE;
-}
 
 BandSPDLinLapackSolver::BandSPDLinLapackSolver()
 :BandSPDLinSolver(SOLVER_TAGS_BandSPDLinLapackSolver)
 {
-    
+
 }
 
 BandSPDLinLapackSolver::~BandSPDLinLapackSolver()
 {
-    
+
 }
 
-
-#ifdef _WIN32
-extern "C" int  DPBSV(char *UPLO,
-			      int *N, int *KD, int *NRHS, 
-			      double *A, int *LDA, double *B, int *LDB, 
-			      int *INFO);
-
-extern "C" int  DPBTRS(char *UPLO,
-			       int *N, int *KD, int *NRHS, 
-			       double *A, int *LDA, double *B, int *LDB, 
-			       int *INFO);
-#else
-
-extern "C" int dpbsv_(char *UPLO, int *N, int *KD, int *NRHS, 
-		      double *A, int *LDA, double *B, int *LDB, 
-		      int *INFO);
-
-extern "C" int dpbtrs_(char *UPLO, int *N, int *KD, int *NRHS, 
-		       double *A, int *LDA, double *B, int *LDB, 
-		       int *INFO);
-
-#endif
-		       
 
 int
 BandSPDLinLapackSolver::solve(void)
 {
   assert(theSOE != nullptr);
-  //if (theSOE == 0) {
-  //    opserr << "WARNING BandSPDLinLapackSolver::solve(void)- ";
-  //    opserr << " No LinearSOE object has been set\n";
-  //    return -1;
-  //}
 
     int n = theSOE->size;
     int kd = theSOE->half_band -1;
@@ -100,55 +59,44 @@ BandSPDLinLapackSolver::solve(void)
 
     // first copy B into X
     for (int i=0; i<n; i++)
-	*(Xptr++) = *(Bptr++);
+      *(Xptr++) = *(Bptr++);
     Xptr = theSOE->X;
 
     // now solve AX = Y
 
-	
 
-#ifdef _WIN32
+    char tflag[] = "U";
     if (theSOE->factored == false) {
-	// factor and solve 	
-	unsigned int sizeC = 1;
-	DPBSV("U", &n,&kd,&nrhs,Aptr,&ldA,Xptr,&ldB,&info);	
+      // factor and solve
+      DPBSV(tflag, &n,&kd,&nrhs,Aptr,&ldA,Xptr,&ldB,&info);
+
+    } else {
+      // solve only using factored matrix
+      // unsigned int sizeC = 1;
+      // DPBTRS("U", sizeC, &n,&kd,&nrhs,Aptr,&ldA,Xptr,&ldB,&info);
+
+        DPBTRS(tflag, &n,&kd,&nrhs,Aptr,&ldA,Xptr,&ldB,&info);
     }
-      else {
-	// solve only using factored matrix	  
-	unsigned int sizeC = 1;	
-	//DPBTRS("U", sizeC, &n,&kd,&nrhs,Aptr,&ldA,Xptr,&ldB,&info);	
-	DPBTRS("U", &n,&kd,&nrhs,Aptr,&ldA,Xptr,&ldB,&info);
-    }
-#else	
-    { if (theSOE->factored == false)          
-	dpbsv_("U",&n,&kd,&nrhs,Aptr,&ldA,Xptr,&ldB,&info);
-      else
-	dpbtrs_("U",&n,&kd,&nrhs,Aptr,&ldA,Xptr,&ldB,&info);
-    }
-#endif    
+
 
     // check if successful
     if (info != 0) {
       if (info > 0) {
-	// opserr << "WARNING BandSPDLinLapackSolver::solve() -";
-	// opserr << "factorization failed, matrix singular U(i,i) = 0, i= " << info-1 << endln;
 	return -info+1;
       } else {
-	// opserr << "WARNING BandSPDLinLapackSolver::solve() - OpenSees code error\n";
 	return info;
-      }      
+      }
     }
 
     theSOE->factored = true;
     return 0;
 }
-    
 
 
 int
 BandSPDLinLapackSolver::setSize()
 {
-  // nothing to do    
+  // nothing to do
   return 0;
 }
 
@@ -162,12 +110,11 @@ BandSPDLinLapackSolver::sendSelf(int cTag,
 
 int
 BandSPDLinLapackSolver::recvSelf(int tag,
-				 Channel &theChannel, 
+				 Channel &theChannel,
 				 FEM_ObjectBroker &theBroker)
 {
   // nothing to do
   return 0;
 }
-
 
 
