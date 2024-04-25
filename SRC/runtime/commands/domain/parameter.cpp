ee9a34b1555bafe3e2acbd675331d5858877f726
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-04-24 19:24:12 -0700

    cmp

--- SRC/runtime/commands/domain/parameter.cpp
+++ SRC/runtime/commands/domain/parameter.cpp
@@ -35,6 +35,7 @@ class TclBasicBuilder;
 #include <Parameter.h>
 #include <ParameterIter.h>
 #include <ElementParameter.h>
+#include <ElementStateParameter.h>
 
 #include <NodeResponseParameter.h>
 #include <LoadFactorParameter.h>
@@ -394,3 +395,98 @@ getParamValue(ClientData clientData, Tcl_Interp *interp, int argc,
   return TCL_OK;
 }
 
+
+int
+setParameter(ClientData clientData, Tcl_Interp *interp, int argc,
+             TCL_Char ** const argv)
+{
+  Domain *theDomain = (Domain*)clientData;
+
+  int argLoc = 1;
+  double newValue = 0.0;
+  ID eleIDs(0, 32);
+  int numEle = 0;
+  int flag = 0;
+
+  if (strstr(argv[argLoc], "-val") != 0) {
+    if (Tcl_GetDouble(interp, argv[argLoc + 1], &newValue) != TCL_OK) {
+      opserr << "WARNING setParameter: invalid parameter value\n";
+      return TCL_ERROR;
+    }
+  } else {
+    opserr << "WARNING setParameter:  -val not found " << endln;
+    return TCL_ERROR;
+  }
+
+  argLoc += 2;
+
+  if (strstr(argv[argLoc], "-ele") != 0) {
+
+    if ((strcmp(argv[argLoc], "-ele") == 0) ||
+        (strcmp(argv[argLoc], "-eles") == 0) ||
+        (strcmp(argv[argLoc], "-element") == 0)) {
+
+      //
+      // read in a list of ele until end of command or other flag
+      //
+
+      argLoc++;
+      int eleTag;
+
+      while (argLoc < argc &&
+             Tcl_GetInt(interp, argv[argLoc], &eleTag) == TCL_OK) {
+        eleIDs[numEle] = eleTag;
+        numEle++;
+        argLoc++;
+      }
+
+      if (numEle > 0)
+        flag = 1;
+
+    } else if (strcmp(argv[argLoc], "-eleRange") == 0) {
+
+      flag = 2;
+
+      // ensure no segmentation fault if user messes up
+      if (argc < argLoc + 3) {
+        opserr << "WARNING recorder Element .. -eleRange start? end?  .. - no "
+                  "ele tags specified\n";
+        return TCL_ERROR;
+      }
+
+      //
+      // read in start and end tags of two elements & add set [start,end]
+      //
+
+      int start, end;
+      if (Tcl_GetInt(interp, argv[argLoc + 1], &start) != TCL_OK) {
+        opserr
+            << "WARNING recorder Element -eleRange start? end? - invalid start "
+            << argv[argLoc + 1] << endln;
+        return TCL_ERROR;
+      }
+      if (Tcl_GetInt(interp, argv[argLoc + 2], &end) != TCL_OK) {
+        opserr
+            << "WARNING recorder Element -eleRange start? end? - invalid end "
+            << argv[argLoc + 2] << endln;
+        return TCL_ERROR;
+      }
+      if (start > end) {
+        int swap = end;
+        end = start;
+        start = swap;
+      }
+      eleIDs[0] = start;
+      eleIDs[1] = end;
+
+      argLoc += 3;
+    }
+
+    ElementStateParameter theParameter(newValue, &argv[argLoc], argc - argLoc,
+                                       flag, &eleIDs);
+
+    theDomain->addParameter(&theParameter);
+  }
+
+  return TCL_OK;
+}
