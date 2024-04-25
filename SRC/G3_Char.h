3aa9c4834d60ee93c8e5beb699930c442e97d428
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-04-25 15:55:40 -0700

    cmp - banner

--- SRC/G3_Char.h
+++ SRC/G3_Char.h
@@ -1,4 +1,13 @@
-
+/* ****************************************************************** **
+**    OpenSees - Open System for Earthquake Engineering Simulation    **
+**          Pacific Earthquake Engineering Research Center            **
+**                                                                    **
+** (C) Copyright 2024, All Rights Reserved.                           **
+**                                                                    **
+** ****************************************************************** */
+//
+// Author: cmp
+//
 #ifndef TCL_Char
 #define TCL_Char const char
 #endif
