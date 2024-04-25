8f4704dfcf59b636235834d57d721e412c6b0372
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-04-25 11:33:59 -0700

    cmp - use `Vector3D`

--- SRC/element/Shell/ASDShellT3LocalCoordinateSystem.h
+++ SRC/element/Shell/ASDShellT3LocalCoordinateSystem.h
@@ -29,7 +29,8 @@
 #ifndef ASDShellT3LocalCoordinateSystem_h
 #define ASDShellT3LocalCoordinateSystem_h
 
-#include <ASDMath.h>
+#include <Quaternion.h>
+#include <Vector3D.h>
 #include <array>
 #include <vector>
 
@@ -43,7 +44,7 @@ class ASDShellT3LocalCoordinateSystem
 
 public:
 
-	typedef ASDVector3<double> Vector3Type;
+	typedef Vector3D Vector3Type;
 
 	typedef ASDQuaternion<double> QuaternionType;
 
