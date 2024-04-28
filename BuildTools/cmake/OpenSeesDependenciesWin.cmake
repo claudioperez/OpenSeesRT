fab7bf634e16a928f8ae0b2e1a47d125e1d59650
Chrystal Chern <52893467+chrystalchern@users.noreply.github.com>
2024-04-27 21:49:58 -0700

    Update OpenSeesDependenciesWin.cmake

--- BuildTools/cmake/OpenSeesDependenciesWin.cmake
+++ BuildTools/cmake/OpenSeesDependenciesWin.cmake
@@ -49,11 +49,12 @@ set(BUNDLE_LIBS "${PROJECT_SOURCE_DIR}/Win64/lib/debug/")
 #  LIBRARY "${BUNDLE_LIBS}/blas.lib"
 # )
 
-opensees_load(CBLAS
-  LIBRARY "${BUNDLE_LIBS}/cblas.lib"
-)
-# set(USE_STATIC_MKL FALSE)
-# set(BLA_STATIC ON)
+# opensees_load(CBLAS
+#   LIBRARY "${BUNDLE_LIBS}/cblas.lib"
+# )
+
+set(USE_STATIC_MKL TRUE)
+set(BLA_STATIC ON)
 set(BLA_VENDOR Intel10_64lp)
 # set(BLA_VENDOR Intel10_64ilp)
 set (MKL_LPATH ${MKL_ROOT}/lib/intel64)
