22cf5fe7035fdb8c60dc498bd0886a49e333fb85
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-05-16 23:23:39 -0700

    cmp - formatting

--- SRC/system_of_eqn/linearSOE/cg/ConjugateGradientSolver.cpp
+++ SRC/system_of_eqn/linearSOE/cg/ConjugateGradientSolver.cpp
@@ -17,13 +17,7 @@
 **   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
 **                                                                    **
 ** ****************************************************************** */
-                                                                        
-// $Revision: 1.2 $
-// $Date: 2003-02-14 23:02:01 $
-// $Source: /usr/local/cvs/OpenSees/SRC/system_of_eqn/linearSOE/cg/ConjugateGradientSolver.cpp,v $
-                                                                        
-                                                                        
-// File: ~/system_of_eqn/linearSOE/cg/ConjugateGradientSolver.C
+//
 //
 // Written: fmk 
 // Created: 06/00
