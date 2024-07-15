a4fa6b42a71e1d52335503b8bb2b2161efa00a9a
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-07-14 18:33:44 -0700

    cmp - change banner

--- SRC/runtime/commands/interpreter.cpp
+++ SRC/runtime/commands/interpreter.cpp
@@ -1,7 +1,8 @@
-/* ****************************************************************** **
-**    OpenSees - Open System for Earthquake Engineering Simulation    **
-**          Pacific Earthquake Engineering Research Center            **
-** ****************************************************************** */
+//===----------------------------------------------------------------------===//
+//
+//        OpenSees - Open System for Earthquake Engineering Simulation
+//
+//===----------------------------------------------------------------------===//
 //
 // Description: This file contains basic commands that enhance the
 // experience of the interpreter. This file should not reference
