f863beebe72c02d50d4520946211d8e9186b542b
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-07-14 17:16:45 -0700

    cmp - cleaning up tcl

--- SRC/runtime/commands/commands.cpp
+++ SRC/runtime/commands/commands.cpp
@@ -1,7 +1,8 @@
-/* ****************************************************************** **
-**    OpenSees - Open System for Earthquake Engineering Simulation    **
-**          Pacific Earthquake Engineering Research Center            **
-** ****************************************************************** */
+//===----------------------------------------------------------------------===//
+//
+//        OpenSees - Open System for Earthquake Engineering Simulation
+//
+//===----------------------------------------------------------------------===//
 //
 // Description: This file contains the functions that will be called by
 // the interpreter when the appropriate command name is specified.
--- SRC/runtime/commands/commands.h
+++ SRC/runtime/commands/commands.h
@@ -1,9 +1,8 @@
-/* ****************************************************************** **
-**    OpenSees - Open System for Earthquake Engineering Simulation    **
-**          Pacific Earthquake Engineering Research Center            **
-** ****************************************************************** */
+//===----------------------------------------------------------------------===//
 //
+//        OpenSees - Open System for Earthquake Engineering Simulation
 //
+//===----------------------------------------------------------------------===//
 //
 
 // domain/element.cpp
