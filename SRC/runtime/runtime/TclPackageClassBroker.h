8b26dfea22161b92de07afd2589dc7c6fa253067
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-07-14 23:01:44 -0700

    cmp - update banner

--- SRC/runtime/runtime/TclPackageClassBroker.h
+++ SRC/runtime/runtime/TclPackageClassBroker.h
@@ -1,29 +1,10 @@
-/* ****************************************************************** **
-**    OpenSees - Open System for Earthquake Engineering Simulation    **
-**          Pacific Earthquake Engineering Research Center            **
-**                                                                    **
-**                                                                    **
-** (C) Copyright 1999, The Regents of the University of California    **
-** All Rights Reserved.                                               **
-**                                                                    **
-** Commercial use of this program without express permission of the   **
-** University of California, Berkeley, is strictly prohibited.  See   **
-** file 'COPYRIGHT'  in main directory for information on usage and   **
-** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
-**                                                                    **
-** Developed by:                                                      **
-**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
-**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
-**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
-**                                                                    **
-** ****************************************************************** */
-
-// $Revision: 1.5 $
-// $Date: 2009-05-14 22:52:04 $
-// $Source:
-// /usr/local/cvs/OpenSees/SRC/actor/objectBroker/TclPackageClassBroker.h,v $
-
-// Written: fmk
+//===----------------------------------------------------------------------===//
+//
+//        OpenSees - Open System for Earthquake Engineering Simulation
+//
+//===----------------------------------------------------------------------===//
+//
+// Written: cmp
 // Revision: A
 //
 // Purpose: This file contains the class definition for FEM_ObjectBroker.
