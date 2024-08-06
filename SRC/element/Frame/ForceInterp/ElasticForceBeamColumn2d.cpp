0ea8cc422ca0cb288df69645bdfddbf557333820
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-08-05 20:48:01 -0700

    Update ElasticForceBeamColumn2d.cpp

--- SRC/element/Frame/ForceInterp/ElasticForceBeamColumn2d.cpp
+++ SRC/element/Frame/ForceInterp/ElasticForceBeamColumn2d.cpp
@@ -67,9 +67,7 @@ Journal of Structural Engineering, Approved for publication, February 2007.
 #include <Domain.h>
 #include <Channel.h>
 #include <FEM_ObjectBroker.h>
-#include <Renderer.h>
 #include <math.h>
-#include <elementAPI.h>
 #include <ElementResponse.h>
 #include <ElementalLoad.h>
 
@@ -77,71 +75,6 @@ Matrix ElasticForceBeamColumn2d::theMatrix(6,6);
 Vector ElasticForceBeamColumn2d::theVector(6);
 double ElasticForceBeamColumn2d::workArea[200];
 
-void * OPS_ADD_RUNTIME_VPV(OPS_ElasticForceBeamColumn2d)
-{
-    if(OPS_GetNumRemainingInputArgs() < 5) {
-	opserr<<"insufficient arguments:eleTag,iNode,jNode,transfTag,integrationTag <-mass mass> <-cmass>\n";
-	return 0;
-    }
-
-    // inputs: 
-    int iData[5];
-    int numData = 5;
-    if(OPS_GetIntInput(&numData,&iData[0]) < 0) {
-	opserr<<"WARNING: invalid integer inputs\n";
-	return 0;
-    }
-
-    // options
-    double mass = 0.0;
-    numData = 1;
-    while(OPS_GetNumRemainingInputArgs() > 0) {
-	const char* type = OPS_GetString();
-	if(strcmp(type,"-mass") == 0) {
-	    if(OPS_GetNumRemainingInputArgs() > 0) {
-		if(OPS_GetDoubleInput(&numData,&mass) < 0) {
-		    opserr<<"WARNING: invalid mass\n";
-		    return 0;
-		}
-	    }
-	}
-    }
-
-    // check transf
-    CrdTransf* theTransf = G3_getSafeBuilder(rt)->getTypedObject<CrdTransf>(iData[3]);
-    if(theTransf == 0) {
-	opserr<<"coord transfomration not found\n";
-	return 0;
-    }
-
-    // check beam integrataion
-    BeamIntegrationRule* theRule = G3_getSafeBuilder(rt)->getTypedObject<BeamIntegrationRule>(iData[4]);
-    if(theRule == 0) {
-	opserr<<"beam integration not found\n";
-	return 0;
-    }
-    BeamIntegration* bi = theRule->getBeamIntegration();
-    if(bi == 0) {
-	opserr<<"beam integration is null\n";
-	return 0;
-    }
-
-    // check sections
-    const ID& secTags = theRule->getSectionTags();
-    SectionForceDeformation** sections = new SectionForceDeformation *[secTags.Size()];
-    for(int i=0; i<secTags.Size(); i++) {
-	sections[i] = OPS_getSectionForceDeformation(secTags(i));
-	if(sections[i] == 0) {
-	    opserr<<"section "<<secTags(i)<<"not found\n";
-		delete [] sections;
-	    return 0;
-	}
-    }
-    
-    Element *theEle =  new ElasticForceBeamColumn2d(iData[0],iData[1],iData[2],secTags.Size(),sections,*bi,*theTransf,mass);
-    delete [] sections;
-    return theEle;
-}
 
 // constructor:
 // invoked by a FEM_ObjectBroker, recvSelf() needs to be invoked on this object.
