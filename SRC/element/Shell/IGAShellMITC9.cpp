074f3fc7c73e4e31bd7d877502a04707cdb3f22a
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-08-31 18:09:41 -0700

    cmp

--- SRC/element/Shell/IGAShellMITC9.cpp
+++ SRC/element/Shell/IGAShellMITC9.cpp
@@ -1,26 +1,11 @@
 /* ****************************************************************** **
 **    OpenSees - Open System for Earthquake Engineering Simulation    **
 **          Pacific Earthquake Engineering Research Center            **
-**                                                                    **
-**                                                                    **
-** (C) Copyright 1999, The Regents of the University of California    **
-** All Rights Reserved.                                               **
-**                                                                    **
-** Commercial use of this program without express permission of the   **
-** University of California, Berkeley, is strictly prohibited.  See   **
-** file 'COPYRIGHT'  in main directory for information on usage and   **
-** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
-**                                                                    **
-** Developed by:                                                      **
-**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
-**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
-**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
-**                                                                    **
 ** ****************************************************************** */
-                                                                        
-// Written: Leopoldo Tesser, Diego Talledo
+//                                                          
+// Adapted from: Leopoldo Tesser, Diego Talledo
 // 9-node lagrandian shell element with membrane and drill
-
+//
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <math.h> 
