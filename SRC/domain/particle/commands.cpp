c2ce715b21eec252a4b844ec6c9e5c1486d80450
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-09-06 17:27:10 -0700

    cmp

--- SRC/domain/particle/commands.cpp
+++ SRC/domain/particle/commands.cpp
@@ -8,18 +8,47 @@ Tcl_PeridynamicsCommands(ClientData cd, Tcl_Interp* interp,
                          int argc, const char** const argv)
 {
 
-  // allocate a new domain
-  ParticleDomain *domain = new ParticleDomain(1, 2);
-
-
+  //
   printf("Hello world\n");
-
 
   // Loop over the command argument array argv and print each one
   for (int i=0; i<argc; i++)
     printf("%s\n", argv[i]);
 
 
+
+
+  //
+  // Parse arguments and create a domain
+  //
+  // Note that argv[0] holds the name of the command.
+  
+  // default values
+  int totnode = 3;
+  int maxfam  = 3;
+
+  // if the size of argv is greater than 1, convert
+  // the second element of the array (ie argv[1]) to an integer
+  // and store it in totnode
+  if (argc > 1) {
+    // If this returns TCL_ERROR it means that argv[1] couldnt be
+    // parsed as an integer.
+    if (Tcl_GetInt(interp, argv[1], &totnode) == TCL_ERROR) {
+      printf("ERROR: Couldnt parse argv[1] as an integer\n");
+    }
+  }
+
+  // Now do the same for the third argument (argv[2])
+  if (argc > 2) {
+    if (Tcl_GetInt(interp, argv[2], &maxfam) == TCL_ERROR) {
+      printf("ERROR: Couldnt parse argv[2] as an integer\n");
+    }
+  }
+
+  // Allocate a new domain
+  ParticleDomain *domain = new ParticleDomain(totnode, maxfam);
+
+  // Print the domain
   domain->print(0);
 
   // clean up memory
