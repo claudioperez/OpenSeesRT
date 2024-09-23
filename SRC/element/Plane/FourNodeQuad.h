ca51a9dc90c96cdca056d5495a53855a01013a9c
Wensley Rushing <179432206+wensley-rushing@users.noreply.github.com>
2024-09-22 23:42:28 -0700

    wr - merge sensitivity elements

--- SRC/element/Plane/FourNodeQuad.h
+++ SRC/element/Plane/FourNodeQuad.h
@@ -17,13 +17,14 @@
 **   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
 **                                                                    **
 ** ****************************************************************** */
-// 
-// Written: MHS
-// Created: Feb 2000
-// Revised: Dec 2000 for efficiency
 //
 // Description: This file contains the class definition for FourNodeQuad.
 //
+// Written: MHS
+// Created: Feb 2000
+// Revised: Dec 2000 for efficiency
+// Sensitivity by Quan Gu, Michele Barbato, Joel P. Conte @ UCSD.  2009 July.
+// 
 #ifndef FourNodeQuad_h
 #define FourNodeQuad_h
 
@@ -42,57 +43,62 @@ class FourNodeQuad : public Element,
 {
   public:
     FourNodeQuad(int tag, int nd1, int nd2, int nd3, int nd4,
-		 NDMaterial &m, const char *type,
-		 double t, double pressure = 0.0, 
-		 double rho = 0.0,
-		 double b1 = 0.0, double b2 = 0.0);
+                 NDMaterial &m, const char *type,
+                 double t, double pressure = 0.0, 
+                 double rho = 0.0,
+                 double b1 = 0.0, double b2 = 0.0);
 
     FourNodeQuad();
     ~FourNodeQuad();
 
-    const char *getClassType(void) const {return "FourNodeQuad";};
+    const char *getClassType() const {return "FourNodeQuad";};
     static constexpr const char* class_name = "FourNodeQuad";
 
-    int getNumExternalNodes(void) const;
-    const ID &getExternalNodes(void);
-    Node **getNodePtrs(void);
+    int getNumExternalNodes() const;
+    const ID &getExternalNodes();
+    Node **getNodePtrs();
 
-    int getNumDOF(void);
+    int getNumDOF();
     void setDomain(Domain *theDomain);
 
     // public methods to set the state of the element    
-    int commitState(void);
-    int revertToLastCommit(void);
-    int revertToStart(void);
-    int update(void);
+    int commitState();
+    int revertToLastCommit();
+    int revertToStart();
+    int update();
 
     // public methods to obtain stiffness, mass, damping and residual information    
-    const Matrix &getTangentStiff(void);
-    const Matrix &getInitialStiff(void);    
-    const Matrix &getMass(void);    
+    const Matrix &getTangentStiff();
+    const Matrix &getInitialStiff();    
+    const Matrix &getMass();    
 
     void zeroLoad();
     int addLoad(ElementalLoad *theLoad, double loadFactor);
     int addInertiaLoadToUnbalance(const Vector &accel);
 
-    const Vector &getResistingForce(void);
-    const Vector &getResistingForceIncInertia(void);            
+    const Vector &getResistingForce();
+    const Vector &getResistingForceIncInertia();            
 
     // Public methods for element output
 
     Response *setResponse(const char **argv, int argc, OPS_Stream &s);
     int getResponse(int responseID, Information &eleInformation);
 
-    int setParameter(const char **argv, int argc, Parameter &param);
-    int updateParameter(int parameterID, Information &info);
-
     // Inherited from TaggedObject
     void Print(OPS_Stream &s, int flag =0);
 
     // Inherited from MovableObject
-    int sendSelf(int commitTag, Channel &theChannel);
-    int recvSelf(int commitTag, Channel &theChannel, FEM_ObjectBroker 
-		  &theBroker);
+    int sendSelf(int tag, Channel &);
+    int recvSelf(int tag, Channel &, FEM_ObjectBroker &);
+
+    // Sensitivity
+    int setParameter(const char **argv, int argc, Parameter &param);
+    int updateParameter(int parameterID, Information &info);
+    int            activateParameter           (int parameterID);
+    const Vector & getResistingForceSensitivity(int gradNumber);
+//  const Matrix & getKiSensitivity            (int gradNumber);
+//  const Matrix & getMassSensitivity          (int gradNumber);
+    int            commitSensitivity           (int gradNumber, int numGrads);
 
     // RWB; PyLiq1 & TzLiq1 need to see the excess pore pressure and initial stresses.
     friend class PyLiq1;
@@ -105,37 +111,43 @@ class FourNodeQuad : public Element,
     constexpr static int NDM = 2;    // number of spatial dimensions
     constexpr static int NEN = 4;    // number of nodes
     constexpr static int NDF = 2;    // number of DOFs per node
+    constexpr static int NIP = 4;    // number of integration points
 
-    // private attributes - a copy for each object of the class
+    //
+    // private member functions 
+    // - only objects of this class can call these
+    double shapeFunction(double xi, double eta);
+    void setPressureLoadAtNodes();
 
+    //
+    // private attributes 
+    // - a copy for each object of the class
     NDMaterial **theMaterial; // pointer to the ND material objects
     
     ID connectedExternalNodes; // Tags of quad nodes
 
     Node *theNodes[NEN];
 
-    static double matrixData[64];  // array data for matrix
-    static Matrix K;		// Element stiffness, damping, and mass Matrix
-    static Vector P;		// Element resisting force vector
-    Vector Q;		        // Applied nodal loads
-    double b[2];		// Body forces
-
-    double appliedB[2]; // Body forces applied with load pattern, C.McGann, U.Washington
-    int applyLoad;      // flag for body force in load, C.McGann, U.Washington
-	
-    Vector pressureLoad;	// Pressure load at nodes
-
-    double thickness;	        // Element thickness
-    double pressure;	        // Normal surface traction (pressure) over entire element
-					 // Note: positive for outward normal
-    double rho;
-    static double shp[3][NEN];	// Stores shape functions and derivatives (overwritten)
-
-    // private member functions - only objects of this class can call these
-    double shapeFunction(double xi, double eta);
-    void setPressureLoadAtNodes(void);
-
     Matrix *Ki;
+    static double matrixData[64];   // array data for matrix
+    static Matrix K;                // Element stiffness, damping, and mass Matrix
+    static Vector P;                // Element resisting force vector
+    Vector Q;                       // Applied nodal loads
+    double b[2];                    // Body forces
+
+    double appliedB[2];             // Body forces applied with load pattern, C.McGann, U.Washington
+    int applyLoad;                  // flag for body force in load
+        
+    Vector pressureLoad;        // Pressure load at nodes
+
+    double thickness;                // Element thickness
+    double pressure;                 // Normal surface traction (pressure) over entire element
+                                     // Note: positive for outward normal
+    double rho;
+    static double shp[3][NEN];       // shape functions and derivatives
+
+    int parameterID;
+
 };
 
 #endif
