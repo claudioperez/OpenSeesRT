9c14e4d083fcc1afb1812d014c3f222c62d260ec
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-09-26 21:13:19 -0700

    cmp

--- SRC/domain/peri/PeriParticle.h
+++ SRC/domain/peri/PeriParticle.h
@@ -7,14 +7,16 @@
 // VectorND and MatrixND are not required here
 // because the linear algebra operations are not used
 // --------------------------------------------
-// #include <VectorND.h>
+#include <VectorND.h>
 // #include <MatrixND.h>
-// using OpenSees::VectorND;
+using OpenSees::VectorND;
 // using OpenSees::MatrixND;
 // ============================================
 template <int ndim>
 class PeriParticle {
 public:
+//using Array = std::array<double, ndim>;
+  using Array = VectorND<ndim>;
 
   // // Print a representation of the domain
   // void print(int flag);
@@ -23,14 +25,15 @@ public:
   // ============================================
   int                 numfam;
   std::vector<int>    nodefam;
-  std::array<double, ndim> coord;
   std::vector<double> vol;
-  double              vol_h;
   std::vector<double> correction, bond_dmg;
+
+  double              vol_h;
+  Array coord;
+  Array bforce, bdisp, pforce;
+  Array disp, vel, acc;
   std::array<int, ndim>    is_force_bound, is_disp_bound;
-  std::array<double, ndim> bforce, bdisp, pforce;
   std::array<double, (ndim-1)*3> stress, strain;
-  std::array<double, ndim> disp, vel, acc;
   double              energy;
   std::array<double, 6> misc;
 };
