af7e6608ed2bffe29a2e3c6a04a3429aa5c7abee
Xuan Hu <106218668+XuanHu0222@users.noreply.github.com>
2024-09-27 00:24:29 -0700

    xh: NosbProj complted

--- SRC/domain/peri/nosb/Mate.h
+++ SRC/domain/peri/nosb/Mate.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <Matrix3D.h>
 using OpenSees::Matrix3D;
 
--- SRC/domain/peri/nosb/NosbBase.h
+++ SRC/domain/peri/nosb/NosbBase.h
@@ -4,12 +4,19 @@
 using namespace OpenSees;
 
 template <int ndim>
-class NosbBase {
-  public:
-  virtual void           init_shape();
-  virtual void           form_trial();
+class NosbBase
+{
+public:
+	// ============================================
+	// MEMBER FUNCTIONS
+	// ============================================
+	virtual void init_shape() = 0;
+	virtual void form_trial() = 0;
+	virtual VectorND<ndim> bond_force(int i, MatrixND<ndim, ndim> &Q) = 0;
+	virtual MatrixND<ndim, ndim> sum_PKinv() = 0;
 
-  virtual MatrixND<ndim,ndim> sum_PKinv();
-  virtual VectorND<ndim> bond_force(int i, MatrixND<ndim,ndim>& Q);
+	// ============================================
+	// MEMBER DATA
+	// ============================================
+	
 };
-
