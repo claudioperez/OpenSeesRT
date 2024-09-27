96a5f29e56cc0f7397f82dcf87aa8f0665dc21f8
Xuan Hu <106218668+XuanHu0222@users.noreply.github.com>
2024-09-26 21:05:47 -0700

    xh: reformate files under peri

--- SRC/domain/peri/PeriDomain.h
+++ SRC/domain/peri/PeriDomain.h
@@ -16,35 +16,36 @@
 // ============================================
 
 template <int ndim>
-class PeriDomain : public PeriDomainBase {
+class PeriDomain : public PeriDomainBase
+{
 public:
-  // ============================================
-  // MEMBER FUNCTIONS
-  // ============================================
+	// ============================================
+	// MEMBER FUNCTIONS
+	// ============================================
 
-  // the special "constructor" member function; no return type declared
-  PeriDomain(int totnode, int maxfam);
-  // override the the getNDM function in PeriDomainBase
-  virtual int getNDM() const { return ndim; } // Return the number of dimensions
+	// the special "constructor" member function; no return type declared
+	PeriDomain(int totnode, int maxfam);
+	// override the the getNDM function in PeriDomainBase
+	virtual int getNDM() const { return ndim; } // Return the number of dimensions
 
-  void set_coord(int i, const std::array<double, ndim>& coord); // Set the coordinates of the particle at index i
+	void set_coord(int i, const std::array<double, ndim> &coord); // Set the coordinates of the particle at index i
 
-  void create_fam(const double delta); // Create families for each particle
+	void create_fam(const double delta); // Create families for each particle
 
-  void set_vols(int i, double vol_i); // Set the volume of the particle i
+	void set_vols(int i, double vol_i); // Set the volume of the particle i
 
-  void calc_vols(const double space); // Calculate the volume of the horizons
+	void calc_vols(const double space); // Calculate the volume of the horizons
 
-  void calc_surf_correction(); // Calculate the surface correction
+	void calc_surf_correction(); // Calculate the surface correction
 
-  void break_bond(const int node1, const int node2); // Break the bond between two particles
+	void break_bond(const int node1, const int node2); // Break the bond between two particles
 
-  // ============================================
-  // MEMBER DATA
-  // ============================================
-  // int       totnode, maxfam;
-  // char      plane_type;
-  std::vector<PeriParticle<ndim>> pts; // container of particles
+	// ============================================
+	// MEMBER DATA
+	// ============================================
+	// int       totnode, maxfam;
+	// char      plane_type;
+	std::vector<PeriParticle<ndim>> pts; // container of particles
 };
 
 #include <PeriDomain.tpp> // Include the implementation of the template class
