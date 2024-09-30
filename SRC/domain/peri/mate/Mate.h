682db7e449f0ba9ca6fa83745eb15a89b2aa13e5
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-09-30 00:08:12 -0700

    cmp - fix missing defs

--- SRC/domain/peri/mate/Mate.h
+++ SRC/domain/peri/mate/Mate.h
@@ -1,3 +1,6 @@
+//
+//
+//
 #pragma once
 #include <Matrix3D.h>
 #include <MatrixSD.h>
@@ -6,13 +9,39 @@ using OpenSees::Matrix3D;
 using OpenSees::MatrixND;
 using OpenSees::MatrixSD;
 
+class Response;
+class Information;
+
 template <int ndim>
 class Mate {
   public:
 
+  virtual int commit()
+  {
+    return 0;
+  }
+
+  virtual int revertToStart()
+  {
+    return 0;
+  }
+
+  virtual int revertToLast()
+  {
+    return 0;
+  }
+#if 0
+  virtual Response *setResponse (const char **argv, int argc, OPS_Stream &s);
+  virtual int getResponse (int responseID, Information &matInformation);
+#endif
   virtual const MatrixSD<ndim>& get_stress() = 0;
 
   virtual void set_strain(const MatrixND<ndim,ndim>&) = 0;
 
+#if 0
+  virtual MatrixSD<ndim> get_tangent()
+  {
+  }
+#endif
 };
 
