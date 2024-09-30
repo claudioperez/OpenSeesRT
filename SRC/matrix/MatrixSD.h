ddcec4d81d05e741f105c1b46ce641ec13f467d9
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-09-30 12:18:18 -0700

    cmp - update symmetric matrix

--- SRC/matrix/MatrixSD.h
+++ SRC/matrix/MatrixSD.h
@@ -1,3 +1,10 @@
+//===----------------------------------------------------------------------===//
+//
+//        OpenSees - Open System for Earthquake Engineering Simulation    
+//    See https://opensees.berkeley.edu/OpenSees/copyright.php for license.
+//
+//===--- MatrixSD.h - Matrix with fixed size ------------------------------===//
+
 #pragma once
 
 namespace OpenSees {
@@ -12,13 +19,64 @@ struct MatrixSD {
 public:
 
   constexpr double& operator()(int i, int j) {
-    return vector[vector_index(i, j, n*(n+1)/2)];
+    return vector[vector_index(i, j, n)];
   }
 
   constexpr const double& operator()(int i, int j) const {
-    return vector[vector_index(i, j, n*(n+1)/2)];
+    return vector[vector_index(i, j, n)];
   }
 
+  constexpr MatrixND<n,n>
+  full() const {
+    MatrixND<n,n> S;
+    for (int i=0; i<n; i++)
+      for (int j=0; j<n; j++)
+        S(i,j) = (*this)(i,j);
+    return S;
+  }
+
+  constexpr friend  VectorND<n>
+  operator*(const MatrixSD<n> &left, const VectorND<n> &right) {
+    VectorND<n> prod;
+    for (index_t i = 0; i < n; ++i) {
+        prod[i] = 0.0;
+        for (index_t k = 0; k < n; ++k) {
+          prod[i] += left(i,k) * right[k];
+        }
+    }
+    return prod;
+  }
+
+
+  template <index_t NR>
+  inline constexpr friend MatrixND<NR, n>
+  operator*(const MatrixND<NR, n> &left, const MatrixSD<n> &right) {
+    MatrixND<NR, n> prod;
+    for (index_t i = 0; i < NR; ++i) {
+      for (index_t j = 0; j < n; ++j) {
+        prod(i, j) = 0.0;
+        for (index_t k = 0; k < n; ++k) {
+          prod(i, j) += left(i,k) * right(k,j);
+        }
+      }
+    }
+    return prod;
+  }
+
+  template <index_t J>
+  inline constexpr friend MatrixND<n, J>
+  operator*(const MatrixSD<n> &left, const MatrixND<n, J> &right) {
+    MatrixND<n, J> prod;
+    for (index_t i = 0; i < n; ++i) {
+      for (index_t j = 0; j < J; ++j) {
+        prod(i, j) = 0.0;
+        for (index_t k = 0; k < n; ++k) {
+          prod(i, j) += left(i,k) * right(k,j);
+        }
+      }
+    }
+    return prod;
+  }
 
 
   template <class MatT, int nk> void 
@@ -32,9 +90,9 @@ private:
   vector_index(int i, int j, int N)
   {
      if (i <= j)
-        return i * N - (i - 1) * i / 2 + j - i;
+        return  i * N - (i - 1) * i / 2 + j - i;
      else
-        return j * N - (j - 1) * j / 2 + i - j;
+        return  j * N - (j - 1) * j / 2 + i - j;
   }
 };
 
