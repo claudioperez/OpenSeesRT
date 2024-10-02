2df3c9525b4598b5eb9bf0c471694e920a4f4f95
Wensley Rushing <179432206+wensley-rushing@users.noreply.github.com>
2024-10-02 16:21:47 -0700

    wr - mark unused to elimitate warnings

--- SRC/handler/DummyStream.h
+++ SRC/handler/DummyStream.h
@@ -46,26 +46,26 @@ class DummyStream : public OPS_Stream
   int write(Vector &data) {return 0;};
 
 
-  OPS_Stream& write(const char *s, int n) {return *this;};
-  OPS_Stream& write(const unsigned char *s, int n) {return *this;};
-  OPS_Stream& write(const signed char *s, int n) {return *this;};
-  OPS_Stream& write(const void *s, int n) {return *this;};
-  OPS_Stream& operator<<(char c) {return *this;};
-  OPS_Stream& operator<<(unsigned char c) {return *this;};
-  OPS_Stream& operator<<(signed char c) {return *this;};
-  OPS_Stream& operator<<(const char *s) {return *this;};
-  OPS_Stream& operator<<(const unsigned char *s) {return *this;};
-  OPS_Stream& operator<<(const signed char *s) {return *this;};
-  OPS_Stream& operator<<(const void *p) {return *this;};
-  OPS_Stream& operator<<(int n) {return *this;};
-  OPS_Stream& operator<<(unsigned int n) {return *this;};
-  OPS_Stream& operator<<(long n) {return *this;};
-  OPS_Stream& operator<<(unsigned long n) {return *this;};
-  OPS_Stream& operator<<(short n) {return *this;};
-  OPS_Stream& operator<<(unsigned short n) {return *this;};
-  OPS_Stream& operator<<(bool b) {return *this;};
-  OPS_Stream& operator<<(double n) {return *this;};
-  OPS_Stream& operator<<(float n) {return *this;};
+  OPS_Stream& write([[maybe_unused]] const char *s, int n) {return *this;};
+  OPS_Stream& write([[maybe_unused]] const unsigned char *s, int n) {return *this;};
+  OPS_Stream& write([[maybe_unused]] const signed char *s, int n) {return *this;};
+  OPS_Stream& write([[maybe_unused]] const void *s, int n) {return *this;};
+  OPS_Stream& operator<<([[maybe_unused]] char c) {return *this;};
+  OPS_Stream& operator<<([[maybe_unused]] unsigned char c) {return *this;};
+  OPS_Stream& operator<<([[maybe_unused]] signed char c) {return *this;};
+  OPS_Stream& operator<<([[maybe_unused]] const char *s) {return *this;};
+  OPS_Stream& operator<<([[maybe_unused]] const unsigned char *s) {return *this;};
+  OPS_Stream& operator<<([[maybe_unused]] const signed char *s) {return *this;};
+  OPS_Stream& operator<<([[maybe_unused]] const void *p) {return *this;};
+  OPS_Stream& operator<<([[maybe_unused]] int n) {return *this;};
+  OPS_Stream& operator<<([[maybe_unused]] unsigned int n) {return *this;};
+  OPS_Stream& operator<<([[maybe_unused]] long n) {return *this;};
+  OPS_Stream& operator<<([[maybe_unused]] unsigned long n) {return *this;};
+  OPS_Stream& operator<<([[maybe_unused]] short n) {return *this;};
+  OPS_Stream& operator<<([[maybe_unused]] unsigned short n) {return *this;};
+  OPS_Stream& operator<<([[maybe_unused]] bool b) {return *this;};
+  OPS_Stream& operator<<([[maybe_unused]] double n) {return *this;};
+  OPS_Stream& operator<<([[maybe_unused]] float n) {return *this;};
 
   int sendSelf(int commitTag, Channel &theChannel) {return 0;};  
   int recvSelf(int commitTag, Channel &theChannel, 
