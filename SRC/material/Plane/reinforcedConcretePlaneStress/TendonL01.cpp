c7142002719d6fb77f150264bce569646ebeb4c7
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-10-02 19:27:03 -0700

    cmp - fix typo

--- SRC/material/Plane/reinforcedConcretePlaneStress/TendonL01.cpp
+++ SRC/material/Plane/reinforcedConcretePlaneStress/TendonL01.cpp
@@ -1223,7 +1223,7 @@ int TendonL01::recvSelf (int commitTag, Channel& theChannel,
                                 FEM_ObjectBroker& theBroker)
 {
    int res = 0;
-   static Vector data(.65);
+   static Vector data(65);
    res = theChannel.recvVector(this->getDbTag(), commitTag, data);
   
    if (res < 0) {
