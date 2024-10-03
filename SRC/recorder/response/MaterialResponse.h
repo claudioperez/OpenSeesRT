51a9f0f8ecbcfbec118f719c8785c42231baf14d
Wensley Rushing <179432206+wensley-rushing@users.noreply.github.com>
2024-10-02 19:33:46 -0700

    wr - nolonger need C++20 for new MaterialResponse

--- SRC/recorder/response/MaterialResponse.h
+++ SRC/recorder/response/MaterialResponse.h
@@ -17,16 +17,18 @@
 **   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
 **                                                                    **
 ** ****************************************************************** */
-                                                                        
-// $Revision: 1.5 $
-// $Date: 2007-03-02 00:12:50 $
-// $Source: /usr/local/cvs/OpenSees/SRC/recorder/response/MaterialResponse.h,v $
-                                                                        
-// Written: MHS 
-// Created: Oct 2000
 //
-// Description: This file contains the MaterialResponse class interface
-
+// This class supercedes the original MaterialResponse class, so that
+// it is only a thin wrapper around the GenericResponse template.
+// Eventually this class should be removed altogether, and only
+// GenericResponse should be used. The prior implementation of 
+// MaterialResponse was the only thing that depended on having a generic
+// Material base class, which was otherwise useless. Migrating to 
+// GenericResponse eliminates entirely the need for Material.
+//
+// Written: cmp
+// Created: Oct 2024
+//
 #ifndef MaterialResponse_h
 #define MaterialResponse_h
 
@@ -43,9 +45,16 @@ class Matrix;
 #if 1
 
 template <typename T>
-using MaterialResponse = GenericResponse<T>;
+class MaterialResponse : public GenericResponse<T> {
+  public:
+  using GenericResponse<T>::GenericResponse;
+};
+template <typename T, typename ...B>
+MaterialResponse(T*, B...)->MaterialResponse<T>;
+
 
 #else
+// Original implementation:
 class MaterialResponse : public Response
 {
  public:
@@ -65,4 +74,6 @@ private:
   int responseID;
 };
 #endif
-#endif
+
+#endif // include guard
+
