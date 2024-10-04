7339df685193ecc461cf7bb387175f84d28c51e8
Wensley Rushing <179432206+wensley-rushing@users.noreply.github.com>
2024-10-03 17:36:14 -0700

    wr

--- SRC/matrix/VectorND.h
+++ SRC/matrix/VectorND.h
@@ -93,320 +93,10 @@ struct VectorND {
       item = value;
   }
 
-
   consteval inline void
   zero() {
     for (T& item : values )
       item = 0.0;
-  }
-
-  int
-  addVector(const T thisFact, const Vector &other, const T otherFact) {
-    if (otherFact == 0.0 && thisFact == 1.0)
-      return 0; 
-
-    else if (thisFact == 1.0) {
-      // want: this += other * otherFact
-      double *dataPtr = values;
-      double *otherDataPtr = other.theData;
-      if (otherFact == 1.0) { // no point doing a multiplication if otherFact == 1.0
-        for (int i=0; i<N; i++) 
-          *dataPtr++ += *otherDataPtr++;
-      } else if (otherFact == -1.0) { // no point doing a multiplication if otherFact == 1.0
-        for (int i=0; i<N; i++) 
-          *dataPtr++ -= *otherDataPtr++;
-      } else 
-        for (int i=0; i<N; i++) 
-          *dataPtr++ += *otherDataPtr++ * otherFact;
-
-    } else if (thisFact == 0.0) {
-        // want: this = other * otherFact
-        double *dataPtr = values;
-        double *otherDataPtr = other.theData;
-        if (otherFact == 1.0) { // no point doing a multiplication if otherFact == 1.0
-          for (int i=0; i<N; i++) 
-            *dataPtr++ = *otherDataPtr++;
-        } else if (otherFact == -1.0) { // no point doing a multiplication if otherFact == 1.0
-          for (int i=0; i<N; i++) 
-            *dataPtr++ = -(*otherDataPtr++);
-        } else 
-          for (int i=0; i<N; i++) 
-            *dataPtr++ = *otherDataPtr++ * otherFact;
-    } else {
-      // want: this = this * thisFact + other * otherFact
-      double *dataPtr = values;
-      double *otherDataPtr = other.theData;
-      if (otherFact == 1.0) { // no point doing a multiplication if otherFact == 1.0
-        for (int i=0; i<N; i++) {
-          double value = *dataPtr * thisFact + *otherDataPtr++;
-          *dataPtr++ = value;
-        }
-      } else if (otherFact == -1.0) { // no point doing a multiplication if otherFact == 1.0
-        for (int i=0; i<N; i++) {
-          double value = *dataPtr * thisFact - *otherDataPtr++;
-          *dataPtr++ = value;
-        }
-      } else 
-        for (int i=0; i<N; i++) {
-          double value = *dataPtr * thisFact + *otherDataPtr++ * otherFact;
-          *dataPtr++ = value;
-      }
-    }
-
-    // successfull
-    return 0;
-  }
-
-  int
-  addVector(const T thisFact, const VectorND<N> &other, const T otherFact) {
-    if (otherFact == 0.0 && thisFact == 1.0)
-      return 0; 
-
-    else if (thisFact == 1.0) {
-      // want: this += other * otherFact
-      double *dataPtr = values;
-      const double * otherDataPtr = other.values;
-      if (otherFact == 1.0) { // no point doing a multiplication if otherFact == 1.0
-        for (int i=0; i<N; i++) 
-          *dataPtr++ += *otherDataPtr++;
-      } else if (otherFact == -1.0) { // no point doing a multiplication if otherFact == 1.0
-        for (int i=0; i<N; i++) 
-          *dataPtr++ -= *otherDataPtr++;
-      } else 
-        for (int i=0; i<N; i++) 
-          *dataPtr++ += *otherDataPtr++ * otherFact;
-
-    } else if (thisFact == 0.0) {
-        // want: this = other * otherFact
-        double *dataPtr = values;
-        const double *otherDataPtr = other.values;
-        if (otherFact == 1.0) {
-          // no point doing a multiplication if otherFact == 1.0
-          for (int i=0; i<N; i++) 
-            *dataPtr++ = *otherDataPtr++;
-        } else if (otherFact == -1.0) {
-          // no point doing a multiplication if otherFact == 1.0
-          for (int i=0; i<N; i++) 
-            *dataPtr++ = -(*otherDataPtr++);
-        } else 
-          for (int i=0; i<N; i++) 
-            *dataPtr++ = *otherDataPtr++ * otherFact;
-    } else {
-      // want: this = this * thisFact + other * otherFact
-      double *dataPtr = values;
-      const double *otherDataPtr = other.values;
-      if (otherFact == 1.0) { // no point doing a multiplication if otherFact == 1.0
-        for (int i=0; i<N; i++) {
-          double value = *dataPtr * thisFact + *otherDataPtr++;
-          *dataPtr++ = value;
-        }
-      } else if (otherFact == -1.0) { // no point doing a multiplication if otherFact == 1.0
-        for (int i=0; i<N; i++) {
-          double value = *dataPtr * thisFact - *otherDataPtr++;
-          *dataPtr++ = value;
-        }
-      } else 
-        for (int i=0; i<N; i++) {
-          double value = *dataPtr * thisFact + *otherDataPtr++ * otherFact;
-          *dataPtr++ = value;
-      }
-    }
-
-    // successfull
-    return 0;
-  }
-
-
-  template <int NC>
-  inline int
-  addMatrixVector(double thisFact, const MatrixND<N, NC, double> &m, const Vector &v, double otherFact)
-  {
-    // check the sizes are compatable
-    assert(NC == v.sz);
-
-    // see if quick return
-    if (thisFact == 1.0 && otherFact == 0.0)
-      return 0;
-
-    else {
-      int incr = 1,
-             i = N,
-             n = NC;
-       DGEMV("N", &i, &n,
-             &otherFact,
-             &m.values[0][0], &i,
-             v.theData, &incr,
-             &thisFact,
-             values,   &incr);
-      // successfull
-      return 0;
-    } 
-  }
-
-  template <int NR>
-  inline int
-  addMatrixTransposeVector(double thisFact, const MatrixND<NR, N, double> &m, const Vector &v, double otherFact)
-  {
-    // check the sizes are compatable
-    assert(NR == v.sz);
-
-    // see if quick return
-    if (thisFact == 1.0 && otherFact == 0.0)
-      return 0;
-
-    else {
-      int incr = 1,
-             i = NR,
-             n = N;
-      DGEMV("T", &i, &n,
-            &otherFact,
-            &m.values[0][0], &i,
-            v.theData, &incr,
-            &thisFact,
-            values,   &incr);
-      // successfull
-      return 0;
-    } 
-  }
-
-
-
-  inline int
-  addMatrixVector(const double thisFact, const Matrix &m, const Vector &v, const double otherFact)
-  {
-    // check the sizes are compatable
-    assert(N == m.noRows());
-    assert(m.noCols() == v.sz);
-
-    // see if quick return
-    if (thisFact == 1.0 && otherFact == 0.0)
-      return 0;
-
-#ifdef VECTOR_BLAS
-    else if (v.sz > 10) {
-      int incr = 1,
-             i = m.numRows,
-             n = m.numCols;
-      return
-        DGEMV("N", &i, &n,
-              &otherFact,
-              m.data, &i,
-              v.theData, &incr,
-              &thisFact,
-              values,   &incr);
-    }
-#endif
-
-    else if (thisFact == 1.0) {
-
-      // want: this += m * v * otherFact
-      if (otherFact == 1.0) { // no point doing multiplication if otherFact = 1.0
-        int otherSize = v.sz;
-        double *matrixDataPtr = m.data;
-        const double *otherDataPtr = v.theData;
-        for (int i=0; i<otherSize; i++) {
-          double otherData = *otherDataPtr++;
-          for (int j=0; j < N; j++)
-            values[j] += *matrixDataPtr++ * otherData;
-        }
-      } 
-      else if (otherFact == -1.0) { // no point doing multiplication if otherFact = -1.0
-        int otherSize = v.sz;
-        double *matrixDataPtr = m.data;
-        const double *otherDataPtr = v.theData;
-        for (int i=0; i<otherSize; i++) {
-          double otherData = *otherDataPtr++;
-          for (int j=0; j < N; j++)
-            values[j] -= *matrixDataPtr++ * otherData;
-        }
-      } 
-      else { // have to do the multiplication
-        int otherSize = v.sz;
-        double *matrixDataPtr = m.data;
-        double *otherDataPtr = v.theData;
-        for (int i=0; i<otherSize; i++) {
-          double otherData = *otherDataPtr++ * otherFact;
-          for (int j=0; j < N; j++)
-            values[j] += *matrixDataPtr++ * otherData;
-        }
-      }
-    }
-
-    else if (thisFact == 0.0) {
-      
-      // want: this = m * v * otherFact
-      for (int i=0; i < N; i++)
-        values[i] = 0.0;
-
-      if (otherFact == 1.0) { // no point doing multiplication if otherFact = 1.0
-        int otherSize = v.sz;
-        const double *matrixDataPtr = m.data;
-        const double *otherDataPtr = v.theData;
-        for (int i=0; i<otherSize; i++) {
-          const double otherData = *otherDataPtr++;
-          for (int j=0; j < N; j++)
-            values[j] += *matrixDataPtr++ * otherData;
-        }
-      } 
-      else if (otherFact == -1.0) { // no point doing multiplication if otherFact = -1.0
-        int otherSize = v.sz;
-        double *matrixDataPtr = m.data;
-        const double *otherDataPtr = v.theData;
-        for (int i=0; i<otherSize; i++) {
-          const double otherData = *otherDataPtr++;
-          for (int j=0; j < N; j++)
-            values[j] -= *matrixDataPtr++ * otherData;
-        }
-      } else {
-        int otherSize = v.sz;
-        double *matrixDataPtr = m.data;
-        double *otherDataPtr = v.theData;
-        for (int i=0; i<otherSize; i++) {
-          double otherData = *otherDataPtr++ * otherFact;
-          for (int j=0; j < N; j++)
-            values[j] += *matrixDataPtr++ * otherData;
-        }
-      }
-    }
-
-    else {
-
-      // want: this = this * thisFact + m * v * otherFact
-      for (int i=0; i<N; i++)
-        values[i] *= thisFact;
-
-      if (otherFact == 1.0) { // no point doing multiplication if otherFact = 1.0
-        int otherSize = v.sz;
-        double *matrixDataPtr = m.data;
-        double *otherDataPtr = v.theData;
-        for (int i=0; i<otherSize; i++) {
-          double otherData = *otherDataPtr++;
-          for (int j=0; j < N; j++)
-            values[j] += *matrixDataPtr++ * otherData;
-        }
-      } else if (otherFact == -1.0) { // no point doing multiplication if otherFact = 1.0
-        int otherSize = v.sz;
-        double *matrixDataPtr = m.data;
-        double *otherDataPtr = v.theData;
-        for (int i=0; i<otherSize; i++) {
-          double otherData = *otherDataPtr++;
-          for (int j=0; j < N; j++)
-            values[j] -= *matrixDataPtr++ * otherData;
-        }
-      } else {
-        int otherSize = v.sz;
-        double *matrixDataPtr = m.data;
-        double *otherDataPtr = v.theData;
-        for (int i=0; i<otherSize; i++) {
-          double otherData = *otherDataPtr++ * otherFact;
-          for (int j=0; j < N; j++)
-            values[j] += *matrixDataPtr++ * otherData;
-        }
-      }
-    }
-
-    // successfull
-    return 0;
   }
 
   template<typename VecT> inline
@@ -516,6 +206,25 @@ struct VectorND {
     return res;
   }
 
+  // Return the cross product this vector with another vector, b.
+  template <class VecB, class VecC> inline 
+  void cross(const VecB& b, VecC& c) const requires(N==3) {
+      c[0] = values[1] * b[2] - values[2] * b[1];
+      c[1] = values[2] * b[0] - values[0] * b[2];
+      c[2] = values[0] * b[1] - values[1] * b[0];
+      return c;
+  }
+
+
+  template <class Vec3T> inline
+  VectorND<N> cross(const Vec3T& b) const requires(N==3) {
+      VectorND<3> c;
+      c[0] = values[1] * b[2] - values[2] * b[1];
+      c[1] = values[2] * b[0] - values[0] * b[2];
+      c[2] = values[0] * b[1] - values[1] * b[0];
+      return c;
+  }
+
   friend std::ostream &
   operator<<(std::ostream &out, const VectorND &vec) {
     out << "{";
@@ -525,30 +234,10 @@ struct VectorND {
     return out << "}\n";
   }
 
-    /**
-    Returns the cross product this vector with another vector.
-    @param b the other vector
-    @return the cross product.
-    */
-    template <class VecB, class VecC> inline 
-    void cross(const VecB& b, VecC& c) const requires(N==3) {
-        c[0] = values[1] * b[2] - values[2] * b[1];
-        c[1] = values[2] * b[0] - values[0] * b[2];
-        c[2] = values[0] * b[1] - values[1] * b[0];
-        return c;
-    }
+#include "VectorND.tpp"
 
-
-    template <class Vec3T> inline
-    VectorND<N> cross(const Vec3T& b) const requires(N==3) {
-        VectorND<3> c;
-        c[0] = values[1] * b[2] - values[2] * b[1];
-        c[1] = values[2] * b[0] - values[0] * b[2];
-        c[2] = values[0] * b[1] - values[1] * b[0];
-        return c;
-    }
 };
-}
+} // namespace OpenSees
 
 template<int N>
 inline OpenSees::VectorND<N>
