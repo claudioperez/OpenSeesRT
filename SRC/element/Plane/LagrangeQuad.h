b2590435492144d1ab355c1014aadb8ceb5af6d2
Wensley Rushing <179432206+wensley-rushing@users.noreply.github.com>
2024-10-04 22:09:08 -0700

    wr - work on `LagrangeQuad`

--- SRC/element/Plane/LagrangeQuad.h
+++ SRC/element/Plane/LagrangeQuad.h
@@ -1,30 +1,13 @@
 /* ****************************************************************** **
 **    OpenSees - Open System for Earthquake Engineering Simulation    **
 **          Pacific Earthquake Engineering Research Center            **
-**                                                                    **
-**                                                                    **
-** (C) Copyright 1999, The Regents of the University of California    **
-** All Rights Reserved.                                               **
-**                                                                    **
-** Commercial use of this program without express permission of the   **
-** University of California, Berkeley, is strictly prohibited.  See   **
-** file 'COPYRIGHT'  in main directory for information on usage and   **
-** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
-**                                                                    **
-** Developed by:                                                      **
-**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
-**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
-**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
-**                                                                    **
 ** ****************************************************************** */
 //
 // Description: This file contains the class definition for LagrangeQuad.
 //
 // Written: MHS
-// Created: Feb 2000
-// Revised: Dec 2000 for efficiency
 // Sensitivity by Quan Gu, Michele Barbato, Joel P. Conte @ UCSD.  2009 July.
-// 
+//
 #ifndef LagrangeQuad_h
 #define LagrangeQuad_h
 
@@ -37,66 +20,71 @@
 #include <quadrature/GaussLegendre2D.hpp>
 
 #define ELE_TAG_LagrangeQuad 0
+
 class Node;
 class Response;
 
 namespace OpenSees {
-class LagrangeQuad : public Element,
-                     protected GaussLegendre<2,4>
-{
-  public:
-    LagrangeQuad(int tag, 
-                 const std::array<int,4>& nodes,
-                 Mate<2> &m, 
-                 const char *type,
-                 double t, double pressure = 0.0, 
-                 double rho = 0.0,
-                 double b1 = 0.0, double b2 = 0.0);
 
-    LagrangeQuad();
-    ~LagrangeQuad();
+template<int NEN, bool enhanced=false>
+class LagrangeQuad : public Element, protected GaussLegendre<2, 4> {
+public:
+  LagrangeQuad(int tag, const std::array<int, NEN>& nodes, 
+               Mate<2>& m,  
+               double thickness,
+               double pressure = 0.0, 
+               double rho = 0.0, 
+               double b1 = 0.0, double b2 = 0.0);
 
-    const char *getClassType() const {return "LagrangeQuad";}
-    static constexpr const char* class_name = "LagrangeQuad";
+  LagrangeQuad();
+  ~LagrangeQuad();
 
-    int getNumExternalNodes() const;
-    const ID &getExternalNodes();
-    Node **getNodePtrs();
+  const char*
+  getClassType() const
+  {
+    return "LagrangeQuad";
+  }
+  static constexpr const char* class_name = "LagrangeQuad";
 
-    int getNumDOF();
-    void setDomain(Domain *theDomain);
+  int getNumExternalNodes() const;
+  const ID& getExternalNodes();
+  Node** getNodePtrs();
 
-    // public methods to set the state of the element    
-    int commitState();
-    int revertToLastCommit();
-    int revertToStart();
-    int update();
+  int getNumDOF();
+  void setDomain(Domain* theDomain);
 
-    // public methods to obtain stiffness, mass, damping and residual information    
-    const Matrix &getTangentStiff();
-    const Matrix &getInitialStiff();    
-    const Matrix &getMass();    
+  // public methods to set the state of the element
+  int commitState();
+  int revertToLastCommit();
+  int revertToStart();
+  int update();
 
-    void zeroLoad();
-    int addLoad(ElementalLoad *theLoad, double loadFactor);
-    int addInertiaLoadToUnbalance(const Vector &accel);
+  // public methods to obtain stiffness, mass, damping and residual information
+  const Matrix& getTangentStiff();
+  const Matrix& getInitialStiff();
+  const Matrix& getMass();
 
-    const Vector &getResistingForce();
-    const Vector &getResistingForceIncInertia();            
+  void zeroLoad();
+  int addLoad(ElementalLoad* theLoad, double loadFactor);
+  int addInertiaLoadToUnbalance(const Vector& accel);
 
-    // Public methods for element output
+  const Vector& getResistingForce();
+  const Vector& getResistingForceIncInertia();
 
-    Response *setResponse(const char **argv, int argc, OPS_Stream &s);
-    int getResponse(int responseID, Information &eleInformation);
+  // Public methods for element output
 
-    // Inherited from TaggedObject
-    void Print(OPS_Stream &s, int flag =0);
+  Response* setResponse(const char** argv, int argc, OPS_Stream& s);
+  int getResponse(int responseID, Information& eleInformation);
 
-    // Inherited from MovableObject
-    int sendSelf(int tag, Channel &);
-    int recvSelf(int tag, Channel &, FEM_ObjectBroker &);
+  // Inherited from TaggedObject
+  void Print(OPS_Stream& s, int flag = 0);
 
-    // Sensitivity
+  // Inherited from MovableObject
+  int sendSelf(int tag, Channel&);
+  int recvSelf(int tag, Channel&, FEM_ObjectBroker&);
+
+  // Sensitivity
+#if 0
     int setParameter(const char **argv, int argc, Parameter &param);
     int updateParameter(int parameterID, Information &info);
     int            activateParameter           (int parameterID);
@@ -104,58 +92,50 @@ class LagrangeQuad : public Element,
 //  const Matrix & getKiSensitivity            (int gradNumber);
 //  const Matrix & getMassSensitivity          (int gradNumber);
     int            commitSensitivity           (int gradNumber, int numGrads);
+#endif
 
-    // RWB; PyLiq1 & TzLiq1 need to see the excess pore pressure and initial stresses.
-    friend class PyLiq1;
-    friend class TzLiq1;
-    friend class QzLiq1; // Sumeet
+protected:
+private:
+  constexpr static int NDM = 2; // number of spatial dimensions
+  constexpr static int NDF = 2; // number of DOFs per node
+  constexpr static int NIP = 4; // number of integration points
 
-  protected:
-    
-  private:
-    constexpr static int NDM = 2;    // number of spatial dimensions
-    constexpr static int NEN = 4;    // number of nodes
-    constexpr static int NDF = 2;    // number of DOFs per node
-    constexpr static int NIP = 4;    // number of integration points
+  //
+  // private member functions
+  // - only objects of this class can call these
+  double shapeFunction(double xi, double eta);
+  void setPressureLoadAtNodes();
 
-    //
-    // private member functions 
-    // - only objects of this class can call these
-    double shapeFunction(double xi, double eta);
-    void setPressureLoadAtNodes();
+  //
+  // private attributes
+  // - a copy for each object of the class
+  std::array<Mate<2>*, NIP> theMaterial; // pointer to the ND material objects
 
-    //
-    // private attributes 
-    // - a copy for each object of the class
-    std::array<Mate<2> *, NIP> theMaterial; // pointer to the ND material objects
-    
-    ID connectedExternalNodes; // Tags of the nodes
+  ID connectedExternalNodes; // Tags of the nodes
 
-    std::array<Node *, NEN> theNodes;
+  std::array<Node*, NEN> theNodes;
 
-    Matrix *Ki;
-    static double matrixData[64];   // array data for matrix
-    static Matrix K;                // Element stiffness, damping, and mass Matrix
-    static Vector P;                // Element resisting force vector
-    Vector Q;                       // Applied nodal loads
-    double b[2];                    // Body forces
+  Matrix* Ki;
+//double matrixData[64]; // array data for matrix
+//Matrix K;              // Element stiffness, damping, and mass Matrix
+//Vector P;              // Element resisting force vector
+  VectorND<NEN*NDF> Q;   // Applied nodal loads
+  double b[2];           // Body forces
 
-    double appliedB[2];             // Body forces applied with load pattern, C.McGann, U.Washington
-    int applyLoad;                  // flag for body force in load
-        
-    Vector pressureLoad;        // Pressure load at nodes
+  double appliedB[2]; // Body forces applied with load pattern, C.McGann, U.Washington
+  int applyLoad;      // flag for body force in load
 
-    double thickness;                // Element thickness
-    double pressure;                 // Normal surface traction (pressure) over entire element
-                                     // Note: positive for outward normal
-    double rho;
-    static double shp[3][NEN];       // shape functions and derivatives
+  Vector pressureLoad; // Pressure load at nodes
 
-    int parameterID;
+  double thickness; // Element thickness
+  double pressure;  // Normal surface traction (pressure) over entire element
+                    // Note: positive for outward normal
+  double rho;
+  double shp[3][NEN]; // shape functions and derivatives
 
+  int parameterID;
 };
-}
+} // namespace OpenSees
 
 #include "LagrangeQuad.tpp"
 #endif
-
