290b4104de0555b39012b4c1c7294fbd4fc7ec95
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-10-24 09:35:16 -0700

    cmp - clean up `GeneralizedNewmark`

--- SRC/analysis/integrator/Dynamic/GeneralizedNewmark.h
+++ SRC/analysis/integrator/Dynamic/GeneralizedNewmark.h
@@ -24,14 +24,12 @@ class FE_Element;
 class GeneralizedNewmark : public TransientIntegrator
 {
 public:
-    // constructors
     GeneralizedNewmark(double gamma, double beta, 
-            double alphaF, double alpahM,
-            int uflag=1,                   // choose which "u"nknown is solved for: d, v or a
-            int iflag=3,                   // choose how to "i"nitialize the unknown: Dd=0, Dv=0 or Da=0
-            bool aflag=false);
+                       double alphaF, double alpahM,
+                       int uflag=1,                   // choose which "u"nknown is solved for: d, v or a
+                       int iflag=3,                   // choose how to "i"nitialize the unknown: Dd=0, Dv=0 or Da=0
+                       bool aflag=false);
 
-    // destructor
     ~GeneralizedNewmark();
     
     // methods which define what the FE_Element and DOF_Groups add
@@ -40,7 +38,7 @@ public:
     virtual int formNodTangent(DOF_Group *theDof)   final;
     virtual int formEleResidual(FE_Element* theEle) final;
     virtual int formNodUnbalance(DOF_Group* theDof) final;
-    
+
     int domainChanged();    
     int newStep(double deltaT);
     int revertToLastStep();
@@ -49,20 +47,20 @@ public:
     double getCFactor();
 
     const Vector &getVel();
-    
+
+    // MovableObject
     virtual int sendSelf(int commitTag, Channel &theChannel);
     virtual int recvSelf(int commitTag, Channel &theChannel, FEM_ObjectBroker &theBroker);
     
     void Print(OPS_Stream &s, int flag = 0);        
     
-    // AddingSensitivity:BEGIN //////////////////////////////////
+    // Sensitivity
     int revertToStart();
     int formSensitivityRHS(int gradNum);
     int formIndependentSensitivityRHS();
     int saveSensitivity   (const Vector &v, int gradNum, int numGrads);
     int commitSensitivity (int gradNum, int numGrads);  
-    int computeSensitivities( );
-    // AddingSensitivity:END ////////////////////////////////////
+    int computeSensitivities();
 
 protected:
 
@@ -82,13 +80,13 @@ private:
 
     int step;                       // track step number to initialize accelerations
     double dt;                      // store time step to determine step number
-    double c1, c2, c3;              // some constants we need to keep
-    Vector *Ut, *Utdot, *Utdotdot;  // response quantities at time t
-    Vector *Ua, *Uadot, *Uadotdot;  // response quantities at time t+alpha
-    Vector *U, *Udot, *Udotdot;     // response quantities at time t+deltaT
+    double cu, cv, ca;              // some constants we need to keep
+    Vector *Uo, *Vo, *Ao;           // solution at time t
+    Vector *Ua, *Va, *Aa;           // solution at time t+alpha
+    Vector *Un, *Vn, *An;           // solution at time t+deltaT
     bool determiningMass;           // flag to check if just want the mass contribution
 
-    // Adding Sensitivity
+    // Sensitivity
     int sensitivityFlag;
     int gradNumber;
     Vector *massMatrixMultiplicator;
@@ -96,7 +94,6 @@ private:
     int assemblyFlag;
     Vector independentRHS;
     Vector dUn, dVn, dAn;
-    //////////////////////
 };
 
 #endif
