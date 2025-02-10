a8e9e4f917112d5505c94f950e3aec57e5747070
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-02-10 13:53:32 -0800

    Fix construction of `ASDShellQ4`

--- SRC/element/Shell/dispatch.cpp
+++ SRC/element/Shell/dispatch.cpp
@@ -13,14 +13,19 @@
 #include <element/Shell/ShellMITC4Thermal.h>
 #include <element/Shell/ShellNLDKGQThermal.h>
 #include <element/Shell/ShellNLDKGT.h>
+using namespace OpenSees;
 
 Element*
 TclDispatch_newASDShellQ4(ClientData clientData, Tcl_Interp* interp, int argc, TCL_Char** const argv)
 {
   assert(clientData != nullptr);
 
+  Vector3D local_x{};
   bool corotational = false;
-
+  bool use_eas = true;
+  bool use_drill_stab = false;
+  double drilling_stab = 0.01;
+  ASDShellQ4::DrillingDOFMode drill_mode = ASDShellQ4::DrillingDOF_Elastic;
 
   BasicModelBuilder* builder = (BasicModelBuilder*)clientData;
 
@@ -30,26 +35,82 @@ TclDispatch_newASDShellQ4(ClientData clientData, Tcl_Interp* interp, int argc, T
     return nullptr;
   }
 
-  int iData[6];
-  int numData = 6;
-  if (OPS_GetInt(&numData, iData) != 0) {
-    opserr << "WARNING invalid integer tag: element ASDShellQ4 \n";
+  int tag;
+  if (Tcl_GetInt(interp, argv[2], &tag) != TCL_OK) {
+    opserr << "WARNING invalid element tag\n";
     return nullptr;
   }
-
-  if (argc == 7) {
-    const char *type = OPS_GetString();
-    if ((strcmp(type, "-corotational") == 0) ||
-        (strcmp(type, "-Corotational") == 0))
-      corotational = true;
+  int nodes[4];
+  for (int i=3; i<7; i++) {
+    if (Tcl_GetInt(interp, argv[i], &nodes[i-3]) != TCL_OK) {
+      opserr << "WARNING invalid node tag \"" << argv[i] << "\" \n";
+      return nullptr;
+    }
+  }
+  int stag;
+  if (Tcl_GetInt(interp, argv[7], &stag) != TCL_OK) {
+    opserr << "WARNING invalid section tag\n";
+    return nullptr;
+  }
+  for (int i=6; i<argc; i++) {
+      if ((strcmp(argv[i], "-corotational") == 0) ||
+          (strcmp(argv[i], "-Corotational") == 0))
+        corotational = true;
+    
+      else if (strcmp(argv[i], "-noeas") == 0) {
+          use_eas = false;
+      }
+      else if (strcmp(argv[i], "-drillingStab") == 0) {
+          if (drill_mode != ASDShellQ4::DrillingDOF_Elastic) {
+              opserr << "Error: element ASDShellQ4: -drillingStab and -drillingNL options are mutually exclusive\n";
+              return 0;
+          }
+          if (argc < i + 2) {
+              opserr << "Error: drilling stabilization parameter not provided with -drillingStab option\n";
+              return nullptr;
+          }
+          if (Tcl_GetDouble(interp, argv[i+1], &drilling_stab) != TCL_OK) {
+              opserr << "Error: cannot get drilling stabilization parameter with -drillingStab option\n";
+              return nullptr;
+          }
+          drilling_stab = std::max(0.0, std::min(1.0, drilling_stab));
+          drill_mode = ASDShellQ4::DrillingDOF_Elastic;
+          use_drill_stab = true;
+          i++;
+      }
+      else if (strcmp(argv[i], "-drillingNL") == 0) {
+          if (use_drill_stab) {
+              opserr << "Error: element ASDShellQ4: -drillingStab and -drillingNL options are mutually exclusive\n";
+              return 0;
+          }
+          drill_mode = ASDShellQ4::DrillingDOF_NonLinear;
+          drilling_stab = 1.0;
+      }
+      // else if (strcmp(argv[i], "-local") == 0) {
+      //     if (OPS_GetNumRemainingInputArgs() < 3) {
+      //         opserr << "Error: element ASDShellQ4: not enough arguments for -local options (3 components are required)\n";
+      //         return 0;
+      //     }
+      //     for (int i = 0; i < 3; ++i) {
+      //         double local_x_com;
+      //         if (OPS_GetDoubleInput(&numData, &local_x_com) == 0) {
+      //             local_x(i) = local_x_com;
+      //         }
+      //         else {
+      //             opserr << "Error: element ASDShellQ4: cannot get the component " << i + 1 << " for the local X axis\n";
+      //             return 0;
+      //         }
+      //     }
+      // }
   }
 
-  SectionForceDeformation *section = builder->getTypedObject<SectionForceDeformation>(iData[5]);
+  SectionForceDeformation *section = builder->getTypedObject<SectionForceDeformation>(stag);
   if (section == nullptr)
     return nullptr;
 
-  return new ASDShellQ4(iData[0], iData[1], iData[2], iData[3], iData[4],
-                        section, corotational);
+  return new ASDShellQ4(tag, nodes[0], nodes[1], nodes[2], nodes[3],
+                        section, local_x, 
+                        corotational, use_eas, drill_mode, drilling_stab);
 }
 
 
