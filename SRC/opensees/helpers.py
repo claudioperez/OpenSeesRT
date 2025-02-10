9f1e98b36e058a7e5fdbe697568bbf09ef689a98
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-02-10 14:49:51 -0800

    Update helpers.py

--- SRC/opensees/helpers.py
+++ SRC/opensees/helpers.py
@@ -1,4 +1,218 @@
 
+class Analysis:
+    def __init__(self, model):
+        self._model = model
+        self._test = None
+
+        self._integrator = None
+
+    def integrator(self, *args):
+        self._integrator = args
+
+    def increment(self, factor=1.0):
+        self._model.integrator("LoadControl", factor)
+        self._model.analysis("Static")
+        self._model.analyze(1, operation='increment')
+
+    def iteration(self):
+#       self._model.test("FixedNumIter", 1, 1)
+        niter = 1
+        tol = 1e-12
+        self._model.test("EnergyIncr", tol, niter, 5)
+        self._model.analysis("Static")
+        self._model.analyze(1, operation='iteration')
+
+def _buckle_factor(boundary, phi=0):
+    import math
+    if boundary == "pin-pin":
+        return math.pi
+
+    if boundary == "fix-slide":
+        return math.pi
+
+    if boundary == "fix-fix":
+        return 2*math.pi
+
+    if boundary == "fix-pin":
+        import scipy.optimize
+        f = lambda x: math.tan(x) - x/(1 + x**2*phi/12)
+        sol = scipy.optimize.root_scalar(f, x0=0.7, bracket=(math.pi, 1.45*math.pi))
+        if sol.converged:
+            return sol.root
+
+    if boundary == "fix-free":
+        return math.pi/2
+
+    if boundary == "pin-slide":
+        return math.pi/2
+
+def _fix_node(model, node, type):
+    ndf = model.getNDF()
+    reactions = [0 for _ in range(ndf)]
+
+    long, tran, vert = range(3)
+    if ndf == 6:
+        bend = vert+3
+        # always fix out-of-plane rotation, which spins
+        # about the transverse DOF
+        reactions[tran+3] = 1
+        reactions[long+3] = 1 # torsion
+        reactions[2] = 1
+    else:
+        bend = vert
+
+
+    if node > 1:
+        vert = 0
+    else:
+        vert = 1
+
+    if type == "fix":
+        reactions[tran] = 1
+        reactions[long] = 0 if node > 1 else 1
+        reactions[bend] = 1
+
+    elif type == "pin":
+        reactions[tran] = 1
+        reactions[long] = 0 if node > 1 else 1
+        reactions[bend] = 0
+
+    elif type == "slide":
+        reactions[tran] = 0
+        reactions[long] = 0 if node > 1 else 1
+        reactions[bend] = 1
+
+    elif type == "free":
+        pass
+
+    model.fix(node, *reactions)
+    return reactions
+
+class Prism:
+    def __init__(self,
+                 length:    float,
+                 element:   str,
+                 section:   dict,
+                 boundary:  tuple,
+                 geometry:  str = None,
+                 transform: str = None,
+                 divisions: int = 1,
+                 rotation = None):
+
+        self.length    = length
+        self.element   = element
+        self.section   = section
+        self.boundary  = boundary
+        self.geometry  = geometry
+        self.transform = transform
+        self.divisions = divisions
+        self.use_shear = True
+        self.rotation = rotation
+
+    def create_model(self, ndm=3):
+        import opensees.openseespy as ops
+
+        L  = self.length
+        boundary = self.boundary
+
+        # Number of elements discretizing the column
+        ne = self.divisions
+
+        elem_type  = self.element
+        geom_type  = self.transform
+
+        # Number of integration points along each element
+        nIP = 5
+        nn = ne + 1
+
+        model = ops.Model(ndm=ndm)
+
+        for i in range(1, nn+1):
+            x = (i-1)/float(ne)*L
+            if ndm == 3:
+                location = (x, 0.0, 0.0)
+            else:
+                location = (x, 0.0)
+
+            if self.rotation is not None:
+                location = tuple(self.rotation@location)
+
+            model.node(i, location)
+
+            model.mass(i, *[1.0]*model.getNDF())
+
+
+        # Define boundary conditions
+        if isinstance(boundary[0], str):
+            _fix_node(model,  1, boundary[0])
+        else:
+            model.fix(1, boundary[0])
+
+        if isinstance(boundary[1], str):
+            _fix_node(model, nn, boundary[1])
+        else:
+            model.fix(nn, boundary[1])
+
+        #
+        # Define cross-section 
+        #
+        sec_tag = 1
+        properties = []
+        for k,v in self.section.items():
+            properties.append("-" + k)
+            properties.append(v)
+
+        model.section("FrameElastic", sec_tag, *properties)
+
+        # Define geometric transformation
+        geo_tag = 1
+        if ndm == 3:
+            vector = (0,  0, 1)
+#           vector = (0,  -1, 0)
+            if self.rotation is not None:
+                vector = tuple(map(float, self.rotation@vector))
+            # for |_ 
+        else:
+            vector = ()
+
+        model.geomTransf(geom_type, geo_tag, *vector)
+
+        # Define elements
+        for i in range(1, ne+1):
+            if self.geometry is None or self.geometry == "Linear" or "Exact" in elem_type:
+                model.element(elem_type, i, (i, i+1),
+                            section=sec_tag,
+                            transform=geo_tag)
+            else:
+                model.element(elem_type, i, (i, i+1),
+                            section=sec_tag,
+                            order={"Linear": 0, "delta": 1}[self.geometry],
+                            transform=geo_tag)
+
+        return model
+
+    def buckle(self, model):
+        # Define loads
+        if self.use_shear:
+            phi = 12*E*I/(Ay*G*L**2)
+            lam = _buckle_factor(boundary, phi)
+            kL = L/lam
+            euler_load = E*I/kL**2  / (1 + lam**2*phi/12)
+        else:
+            kL = L/_buckle_factor(boundary)
+            euler_load = E*I/kL**2
+
+        model.pattern("Plain", 1, "Linear")
+        if self.ndm == 2:
+            load = (-euler_load, 0.0, 0.0)
+        else:
+            load = (-euler_load, 0.0, 0.0, 0, 0, 0)
+
+        model.load(nn, *load, pattern=1)
+
+        return model
+
+
 def node_average(model, response, ndm=2, keys=None):
     """
     Return a dictionary of stress components per node.
